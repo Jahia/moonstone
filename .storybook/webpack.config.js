e2f24f515c1e55a56396e6368c8f4f5fa4b1b00e
FBerthelot <florent.berthelot35@gmail.com>
2019-09-27 10:20:55 +0200

    add storybook sass

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path')
+
+// Export a function. Accept the base config as the only param.
+module.exports = async ({ config, mode }) => {
+    config.module.rules.push({
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+        include: path.resolve(__dirname, '../')
+    })
+
+    // Return the altered config
+    return config
+}
