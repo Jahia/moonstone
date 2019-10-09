907d349aac36f790e2e92506bd82e2b0a2e77fca
Thomas Draier <tdraier@jahia.com>
2019-10-09 14:00:50 +0200

    MOON-3 : removed comments

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,13 +3,6 @@ import {addDecorator, addParameters, configure} from '@storybook/react';
 import {DocsContainer, DocsPage} from '@storybook/addon-docs/blocks';
 import {GlobalStyle} from '../src/globalstyle';
 
-// automatically import all files ending in *.stories.js
-// const req = require.context('../src', true, /\.stories\.jsx$/);
-
-// function loadStories() {
-//     req.keys().forEach(filename => req(filename));
-// }
-
 addDecorator(story => (
     <>
         <GlobalStyle/>
