0b828cb94e34861cf74b30d2350279753fe47762
Thomas Draier <tdraier@jahia.com>
2019-10-18 11:13:54 +0200

    MOON-6 : Removed expanded as component is self-managed, added local state. Split children in 2 slots top and bottom. Fixed prop-types

--- src/components/PrimaryNav/PrimaryNav.jsx
+++ src/components/PrimaryNav/PrimaryNav.jsx
@@ -1,41 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import styles from './PrimaryNav.scss';
 
-export const PrimaryNav = ({isExpanded, headerLogo, children, headerCaption}) => {
-    console.log({isExpanded, headerLogo, children, headerCaption});
+export const PrimaryNav = ({headerLogo, top, bottom, headerCaption}) => {
+    console.log({headerLogo, top, bottom, headerCaption});
+    const [isExpanded, setExpanded] = useState(false);
+
+    function toggleExpand() {
+        setExpanded(!isExpanded);
+    }
+
     return (
         <div className={classnames(
             styles.primaryNav,
             {[styles.expanded]: isExpanded},
         )}
         >
+            <button type="button" onClick={toggleExpand}>Toggle</button>
             <ul>
-                {children}
+                {top}
+            </ul>
+            <ul>
+                {bottom}
             </ul>
         </div>
     );
 };
 
 PrimaryNav.defaultProps = {
-    isExpanded: false,
     headerLogo: '',
     headerCaption: '',
-    children: null
+    top: [],
+    bottom: []
 };
 
 PrimaryNav.propTypes = {
-
-    /**
-     * Is navigation expanded
-     */
-    isExpanded: PropTypes.bool,
-
     /**
      * Image of logo application
      */
-    headerLogo: PropTypes.any,
+    headerLogo: PropTypes.node,
 
     /**
      * Application's environment
@@ -43,7 +47,12 @@ PrimaryNav.propTypes = {
     headerCaption: PropTypes.string,
 
     /**
-     * Items
+     * Primary nav groups displayed at the top
      */
-    children: PropTypes.any
+    top: PropTypes.arrayOf(PropTypes.element),
+
+    /**
+     * Primary nav groups displayed at the bottom
+     */
+    bottom: PropTypes.arrayOf(PropTypes.element)
 };
