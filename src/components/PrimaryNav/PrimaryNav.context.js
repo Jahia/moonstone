a7ef7e2614803772253027a785436375b48dc30b
alex <alexandrk_@hotmail.com>
2019-11-05 16:10:58 -0500

    BACKLOG-11280 Add menu / header, remove nav items to be completed later

---
+++ src/components/PrimaryNav/PrimaryNav.context.js
@@ -0,0 +1,5 @@
+import {createContext} from 'react';
+
+const PrimaryNavContext = createContext({});
+
+export {PrimaryNavContext};
--- src/components/PrimaryNav/PrimaryNav.jsx
+++ src/components/PrimaryNav/PrimaryNav.jsx
@@ -2,8 +2,45 @@ import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'clsx';
 import styles from './PrimaryNav.scss';
+import {PrimaryNavContext} from './PrimaryNav.context';
+// TODO resolve this issue here
+import Menu from '../../../dist/icons/Menu';
+import ArrowLeft from '../../../dist/icons/ArrowLeft';
 
-// TODO implement the component !
+const NavButton = ({isExpanded, toggleExpand}) => {
+    if (isExpanded) {
+        return (
+            <button type="button" onClick={toggleExpand}>
+                <ArrowLeft/>
+            </button>
+        );
+    }
+
+    return (
+        <button type="button" onClick={toggleExpand}>
+            <Menu/>
+        </button>
+    );
+};
+
+NavButton.propTypes = {
+    isExpanded: PropTypes.bool.isRequired,
+    toggleExpand: PropTypes.func.isRequired
+};
+
+const NavHeader = ({isExpanded, headerCaption}) => {
+    if (isExpanded) {
+        return headerCaption;
+    }
+
+    return null;
+};
+
+NavHeader.propTypes = {
+    isExpanded: PropTypes.bool.isRequired,
+    headerCaption: PropTypes.string.isRequired
+};
+
 export const PrimaryNav = ({headerLogo, top, bottom, headerCaption}) => {
     console.log({headerLogo, top, bottom, headerCaption});
     const [isExpanded, setExpanded] = useState(false);
@@ -13,23 +50,32 @@ export const PrimaryNav = ({headerLogo, top, bottom, headerCaption}) => {
     }
 
     return (
-        <>
+        <PrimaryNavContext.Provider value={{isExpanded: isExpanded}}>
             <nav className={classnames(
                 styles.primaryNav,
                 {[styles.expanded]: isExpanded},
                 'flexCol',
             )}
             >
-                <button type="button" onClick={toggleExpand}>Toggle</button>
+                <div className={classnames('flexRow_nowrap', styles.navHeader)}>
+                    <div className={classnames(styles.navButtonContainer, 'flexRow', 'flexRow_center')}>
+                        <NavButton isExpanded={isExpanded} toggleExpand={toggleExpand}/>
+                    </div>
+                    <div className={classnames('flexRow', 'flexRow_center', 'alignCenter', 'flexFluid')}>
+                        <NavHeader headerCaption={headerCaption} isExpanded={isExpanded}/>
+                    </div>
+                </div>
+
                 <ul className={classnames('flexFluid')}>
                     {top}
                 </ul>
+
                 <ul>
                     {bottom}
                 </ul>
             </nav>
             {isExpanded && <div className={(styles.overlay)} onClick={toggleExpand}/>}
-        </>
+        </PrimaryNavContext.Provider>
     );
 };
 
