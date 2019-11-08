7135eeba6181eca3447810e788a8c260f2a850db
Thomas Draier <tdraier@jahia.com>
2019-11-08 13:51:22 +0100

    MOON-1 : Fix global style loading

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -27,6 +27,7 @@ module.exports = async ({config, mode}) => {
     });
     config.module.rules.push({
         test: /\.scss$/,
+        sideEffects: true,
         use: [
             'style-loader',
             {
