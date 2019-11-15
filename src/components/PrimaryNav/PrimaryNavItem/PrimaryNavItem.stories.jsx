cbe76b3002a6ed6c25fa75e079f54c11d68cc372
cedmail <cmailleux@jahia.com>
2019-11-15 07:17:46 -0500

    BACKLOG-11286: Implement Button in PrimaryNavItem, only ghost and reverse implemented on the button Fix fonts delivery

--- src/components/PrimaryNav/PrimaryNavItem/PrimaryNavItem.stories.jsx
+++ src/components/PrimaryNav/PrimaryNavItem/PrimaryNavItem.stories.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import {storiesOf} from '@storybook/react';
 import {PrimaryNavItem} from './index';
 import Edit from '~/icons/asset/Edit.svg';
+import Person from '~/icons/asset/Person.svg';
+import Power from '~/icons/asset/Power.svg';
 import markdownNotes from './PrimaryNavItem.md';
 import {boolean, withKnobs} from '@storybook/addon-knobs';
+import {Button} from '~/components';
 
 storiesOf('Components|PrimaryNavItem', module)
     .addParameters({
@@ -22,5 +25,7 @@ storiesOf('Components|PrimaryNavItem', module)
             <PrimaryNavItem icon={<Edit/>} label="NavItem not selected (default)"/>
             <PrimaryNavItem isSelected icon={<Edit/>} label="NavItem Selected"/>
             <PrimaryNavItem isSelected={boolean('selected')} icon={<Edit/>} label="Another NavItem"/>
+            <PrimaryNavItem icon={<Person/>} label="My profile" subtitle="username as a subtitle"/>
+            <PrimaryNavItem icon={<Person/>} label="My profile" subtitle="username as a subtitle" button={<Button icon={<Power/>} label="Sign Out" variant="ghost" color="reverse"/>}/>
         </ul>
     ));
