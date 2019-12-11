9ab6de063aecedbeca21dd4e67be3e65cc9bbcdd
alex <alexandrk_@hotmail.com>
2019-12-11 11:59:36 -0500

    MOON-32 Process icon files with babel, whitelist re-resizable library

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ const ComponentsConfig = {
     },
     mode: 'development',
     devtool: 'source-map',
-    externals: [nodeExternals()],
+    externals: [nodeExternals({whitelist: ['re-resizable']})],
     resolve: {
         // Add alias to import files easily
         alias: {
