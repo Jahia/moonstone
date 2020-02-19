5366115f0cb184553be583d74f423c46a678a1f9
alex <alexandrk_@hotmail.com>
2020-02-19 13:08:33 -0500

    BACKLOG-12219 Ignore events

--- src/components/PrimaryNav/PrimaryNav.stories.jsx
+++ src/components/PrimaryNav/PrimaryNav.stories.jsx
@@ -34,7 +34,8 @@ storiesOf('Components|PrimaryNav', module)
                             <PrimaryNavItem label="Very very long long name with many characters" icon={<Feather size="big"/>}/>
                             <PrimaryNavItem icon={<Person size="big"/>} label="My profile" subtitle="username as a subtitle"/>
                             <PrimaryNavItem icon={<Person size="big"/>} label="Very very long long long long label" subtitle="username as a subtitle username as a subtitle username as a subtitle username as a subtitle" button={<Button icon={<Power/>} label="Sign Out" variant="ghost" color="reverse" onClick={() => {}}/>}/>
-                            <PrimaryNavItem icon={<Person/>} label="With badge" badge="33"/>
+                            <PrimaryNavItem icon={<Person size="big"/>} label="With badge" badge="3"/>
+                            <PrimaryNavItem icon={<Person size="big"/>} label="With badge" badge="333"/>
                         </PrimaryNavItemsGroup>
                         <PrimaryNavItemsGroup isDisplayedWhenCollapsed={false}>
                             <PrimaryNavItem url="https://jahia.com" label="Jahia Link"/>
