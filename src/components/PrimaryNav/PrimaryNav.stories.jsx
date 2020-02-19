9f0027707e132a8298113fdad6503ac0363c45b2
alex <alexandrk_@hotmail.com>
2020-02-19 13:00:29 -0500

    BACKLOG-12219 Display badge in primary nav item

--- src/components/PrimaryNav/PrimaryNav.stories.jsx
+++ src/components/PrimaryNav/PrimaryNav.stories.jsx
@@ -31,9 +31,10 @@ storiesOf('Components|PrimaryNav', module)
                             <PrimaryNavItem isSelected label="NavItem selected" icon={<Apps/>}/>
                         </PrimaryNavItemsGroup>
                         <PrimaryNavItemsGroup>
-                            <PrimaryNavItem label="Very very long long name with many characters" icon={<Feather/>}/>
-                            <PrimaryNavItem icon={<Person/>} label="My profile" subtitle="username as a subtitle"/>
-                            <PrimaryNavItem icon={<Person/>} label="Very very long long long long label" subtitle="username as a subtitle username as a subtitle username as a subtitle username as a subtitle" button={<Button icon={<Power/>} label="Sign Out" variant="ghost" color="reverse" onClick={() => {}}/>}/>
+                            <PrimaryNavItem label="Very very long long name with many characters" icon={<Feather size="big"/>}/>
+                            <PrimaryNavItem icon={<Person size="big"/>} label="My profile" subtitle="username as a subtitle"/>
+                            <PrimaryNavItem icon={<Person size="big"/>} label="Very very long long long long label" subtitle="username as a subtitle username as a subtitle username as a subtitle username as a subtitle" button={<Button icon={<Power/>} label="Sign Out" variant="ghost" color="reverse" onClick={() => {}}/>}/>
+                            <PrimaryNavItem icon={<Person/>} label="With badge" badge="33"/>
                         </PrimaryNavItemsGroup>
                         <PrimaryNavItemsGroup isDisplayedWhenCollapsed={false}>
                             <PrimaryNavItem url="https://jahia.com" label="Jahia Link"/>
