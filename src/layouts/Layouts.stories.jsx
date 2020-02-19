5f16429fa7ed3385499be42305f7c81d26407331
alex <alexandrk_@hotmail.com>
2020-02-19 13:12:19 -0500

    BACKLOG-12219 Revert layout (accidental commit)

--- src/layouts/Layouts.stories.jsx
+++ src/layouts/Layouts.stories.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {storiesOf} from '@storybook/react';
 import {withKnobs, number, text, boolean} from '@storybook/addon-knobs';
 
@@ -11,36 +11,31 @@ const resizeWidth = () => number('Set width to resize', 245, {min: 120, max: 496
 
 storiesOf('Layouts|Demos', module)
     .addDecorator(withKnobs)
-    .add('Default', () => {
-        const [secondaryMenuIsOpen, setSecondaryMenuIsOpen] = useState(true);
-
-        return (
-            <div style={{transform: 'scale(1)'}}>
-                <LayoutApp
-                    navigation={
-                        <PrimaryNav isExpanded={boolean('Expand', false, 'Level 1')}>
-                            level 1
-                        </PrimaryNav>
-                    }
-                    content={
-                        <LayoutModule
-                            navigation={
-                                <SecondaryNav resizeWidth={resizeWidth()} isVisible={secondaryMenuIsOpen} onToggleVisible={(e, currentState) => setSecondaryMenuIsOpen(!currentState)}>
-                                    level 2
-                                </SecondaryNav>
-                            }
-                            content={
-                                <div style={{padding: '20px'}}>
-                                    {text('Content', 'My module content', 'Content')}
-                                </div>
-                            }
-                        />
-                    }
-                />
-            </div>
-        );
-    }
-    )
+    .add('Default', () => (
+        <div style={{transform: 'scale(1)'}}>
+            <LayoutApp
+                navigation={
+                    <PrimaryNav isExpanded={boolean('Expand', false, 'Level 1')}>
+                        level 1
+                    </PrimaryNav>
+                }
+                content={
+                    <LayoutModule
+                        navigation={
+                            <SecondaryNav resizeWidth={resizeWidth()}>
+                                level 2
+                            </SecondaryNav>
+                        }
+                        content={
+                            <div style={{padding: '20px'}}>
+                                {text('Content', 'My module content', 'Content')}
+                            </div>
+                        }
+                    />
+                }
+            />
+        </div>
+    ))
 
     .add('Without level 2', () => (
         <div style={{transform: 'scale(1)'}}>
