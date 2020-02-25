342076c29cf728ff38fa5de5b3e5e854ae8726b6
Félix Vallé <31360150+ffffffelix@users.noreply.github.com>
2020-02-25 15:44:31 +0100

    MOON-73: Add vertical variant to the Separator component (#84)
    Co-authored-by: Thomas Draier <tdraier@jahia.com>

--- src/components/Separator/Separator.jsx
+++ src/components/Separator/Separator.jsx
@@ -5,33 +5,48 @@ import styles from './Separator.scss';
 
 export const SeparatorSpacings = ['small', 'medium', 'big'];
 export const SeparatorSizes = ['medium', 'large', 'full'];
+export const SeparatorVariants = ['horizontal', 'vertical'];
 
-export const Separator = ({size, spacing, ...props}) => {
+export const Separator = ({size, spacing, variant, className, ...props}) => {
     return (
         <hr {...props}
             className={classnames(
                 styles.separator,
+                styles[`separator_${variant}`],
                 styles[`size_${size}`],
-                styles[`spacing_${spacing}`]
+                styles[`spacing_${spacing}`],
+                className
             )}
         />
     );
 };
 
 Separator.defaultProps = {
+    variant: 'horizontal',
     size: 'full',
     spacing: 'small'
 };
 
 Separator.propTypes = {
     /**
+     * Variants: Horizontal or Vertical
+     */
+    variant: PropTypes.oneOf(SeparatorVariants),
+
+    /**
      * Vertical spacings
      */
     spacing: PropTypes.oneOf(SeparatorSpacings),
+
     /**
      * Size
      */
-    size: PropTypes.oneOf(SeparatorSizes)
+    size: PropTypes.oneOf(SeparatorSizes),
+
+    /**
+     * Additional classname
+     */
+    className: PropTypes.string
 };
 
 Separator.displayName = 'Separator';
