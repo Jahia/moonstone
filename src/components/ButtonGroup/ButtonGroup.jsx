a2fc9661e09f74715d659fe677f8f1d03c12963d
Félix Vallé <31360150+ffffffelix@users.noreply.github.com>
2020-02-28 15:24:54 +0100

    MOON-61: Create ButtonGroup component (#94)

---
+++ src/components/ButtonGroup/ButtonGroup.jsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './ButtonGroup.scss';
+import classnames from 'clsx';
+import {buttonSizes, buttonVariants, buttonColors} from '~/components/Button';
+
+export const ButtonGroup = ({size, isReversed, variant, color, className, children, ...props}) => {
+    return (
+        <div role="group"
+             className={classnames(
+                styles.buttonGroup,
+                className,
+                'flexRow',
+                'alignCenter'
+            )}
+             {...props}
+        >
+            {
+                React.Children.map(children, button => {
+                    if (!React.isValidElement(button)) {
+                        return null;
+                    }
+
+                    return (
+                        <button.type
+                            {...button.props}
+                            size={size}
+                            variant={variant}
+                            isReversed={isReversed}
+                            color={color}
+                            className={classnames(
+                                styles[`variant_${variant}`],
+                                styles[`color_${color}`]
+                            )}
+                        />
+                    );
+                })
+            }
+        </div>
+    );
+};
+
+ButtonGroup.defaultProps = {
+    size: 'default',
+    variant: 'default',
+    color: 'default',
+    isReversed: false,
+    className: null
+};
+
+ButtonGroup.propTypes = {
+    /**
+     * Buttons grouped
+     */
+    children: PropTypes.node.isRequired,
+
+    /**
+     * Buttons size
+     */
+    size: PropTypes.oneOf(buttonSizes),
+
+    /**
+     * Button style
+     */
+    variant: PropTypes.oneOf(buttonVariants),
+
+    /**
+     * Button color
+     */
+    color: PropTypes.oneOf(buttonColors),
+
+    /**
+     * Is button color reversed
+     */
+    isReversed: PropTypes.bool,
+
+    /**
+     * Additional classname
+     */
+    className: PropTypes.string
+};
+
+ButtonGroup.displayName = 'ButtonGroup';
