de8ca0e2af9b72c2d13371dbba6d40a345463dfb
alex <alexandrk_@hotmail.com>
2020-03-03 10:09:54 -0500

    MOON-82 Add secondary nav header component

---
+++ src/components/SecondaryNav/SecondaryNavHeader/SecondaryNavHeader.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'clsx';
+import styles from './SecondaryNavHeader.scss';
+
+const SecondaryNavHeader = ({children}) => {
+    return (
+        <div className={classnames('.flexCol_center', '.alignCenter', styles.secondaryNavHeader)}>
+            {children}
+        </div>
+    );
+};
+
+SecondaryNavHeader.propTypes = {
+
+    /**
+     * Content of the component
+     */
+    children: PropTypes.node.isRequired
+};
+
+SecondaryNavHeader.displayName = 'SecondaryNavHeader';
+
+export {SecondaryNavHeader};
