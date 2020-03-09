fff71d88c817d5af524441b752b1b0515f139628
Félix Vallé <31360150+ffffffelix@users.noreply.github.com>
2020-03-09 14:22:57 +0100

    MOON-49: Add Breadcrumb component (#108)

---
+++ src/components/Breadcrumb/Breadcrumb.jsx
@@ -0,0 +1,47 @@
+import React, {Fragment} from 'react';
+import PropTypes from 'prop-types';
+import styles from './Breadcrumb.scss';
+import classnames from 'clsx';
+import {ChevronRight} from '~/tokens/icons/asset';
+
+export const Breadcrumb = ({children, className, ...props}) => {
+    const classNames = classnames(
+        styles.breadcrumb,
+        className
+    );
+
+    const allItems = React.Children.toArray(children);
+
+    return (
+        <nav className={classNames} aria-label="breadcrumb" {...props}>
+            <ol className={classnames('flexRow_nowrap', 'alignCenter')}>
+                {
+                    allItems.map((item, index) => (
+                        <Fragment key={`child-${item.props.label}`}>
+                            {item}
+
+                            {index < allItems.length - 1 &&
+                                <li className={classnames(styles.breadcrumb_separator, 'flexRow_center', 'alignCenter')}>
+                                    <ChevronRight aria-hidden/>
+                                </li>}
+                        </Fragment>
+                    ))
+                }
+            </ol>
+        </nav>
+    );
+};
+
+Breadcrumb.propTypes = {
+    /**
+     * Additional classname
+     */
+    className: PropTypes.string,
+
+    /**
+     * Content of the component
+     */
+    children: PropTypes.node
+};
+
+Breadcrumb.displayName = 'Breadcrumb';
