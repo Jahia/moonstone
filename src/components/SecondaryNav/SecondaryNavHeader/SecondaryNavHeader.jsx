b478d53cfd82061081ca990c08c9030d912d244d
Alex Karmanov <akarmanov@jahia.com>
2020-03-18 08:38:53 -0400

    BACKLOG-12813 Make header font size as on figma (#124)
    * BACKLOG-12813 Make header font size as on figma
    
    * BACKLOG-12813 Update component

--- src/components/SecondaryNav/SecondaryNavHeader/SecondaryNavHeader.jsx
+++ src/components/SecondaryNav/SecondaryNavHeader/SecondaryNavHeader.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'clsx';
 import styles from './SecondaryNavHeader.scss';
+import {Typography} from '../../Typography';
 
 const SecondaryNavHeader = ({children}) => (
-    <header className={classnames(styles.secondaryNavHeader, 'flexCol_center', 'alignCenter')}>
+    <Typography component="header"
+                className={classnames(styles.secondaryNavHeader, 'flexCol_center', 'alignCenter')}
+                variant="heading"
+    >
         {children}
-    </header>
+    </Typography>
 );
 
 SecondaryNavHeader.propTypes = {
