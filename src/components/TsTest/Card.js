25a50542e6ce14495c1eb1a548ff6ecc42507027
tdraier <tdraier@jahia.com>
2020-05-27 10:34:27 +0200

    Added ts component

---
+++ src/components/TsTest/Card.js
@@ -0,0 +1,17 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+exports.__esModule = true;
+exports.Card = void 0;
+var react_1 = __importDefault(require("react")); // we need this to make JSX compile
+exports.Card = function (_a) {
+    var title = _a.title, paragraph = _a.paragraph;
+    return <aside>
+  <h2>{title}</h2>
+  <p>
+    {paragraph}
+  </p>
+    </aside>;
+};
+var el = <exports.Card title="Welcome!" paragraph="To this example"/>;
