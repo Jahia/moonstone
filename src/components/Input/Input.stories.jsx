93f95bf7b9b756f1b4cc4362e2c2bace424877cf
Vindhya Raviraj <vindhya.raviraj@gmail.com>
2020-08-25 22:22:57 -0400

    MOON-141: Initial Input component functionality

--- src/components/Input/Input.stories.jsx
+++ src/components/Input/Input.stories.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {storiesOf} from '@storybook/react';
 import {withKnobs} from '@storybook/addon-knobs';
 // Import {select, text, withKnobs} from '@storybook/addon-knobs';
 import classnames from 'clsx';
 import storyStyles from '~/__storybook__/storybook.module.scss';
+import PropTypes from 'prop-types';
 
 import markdownNotes from './Input.md';
 import {Input} from './index';
@@ -15,9 +16,61 @@ storiesOf('Components|Input' +
         notes: {markdown: markdownNotes}
     })
     .addDecorator(withKnobs)
-    .add('Basic', () => (
-        <section className={classnames(storyStyles.storyWrapper)}>
-            <Input/>
-        </section>
+    .add('Testing', () => {
+        const Parent = ({children}) => {
+            const [state, setState] = useState();
+            return <>{children(state, setState)}</>;
+        };
 
-    ));
+        Parent.propTypes = {
+            children: PropTypes.node
+        };
+
+        return (
+            <section className={classnames(storyStyles.storyWrapper)}>
+                <Parent>
+                    {(state, setState) => (
+                        <Input
+                            placeholder="this is a placeholder!"
+                            value={state}
+                            onChange={e => {
+                                setState(e.target.value);
+                            }}
+                            onBlur={() => {
+                                // Console.log('the input was blurred!');
+                            }}
+                            onFocus={() => {
+                                // Console.log('the input was focused!');
+                            }}
+                        />
+                    )}
+                </Parent>
+            </section>
+        );
+    })
+    .add('Read Only', () => {
+        const Parent = ({children}) => {
+            const [state, setState] = useState('this input is read only!');
+            return <>{children(state, setState)}</>;
+        };
+
+        Parent.propTypes = {
+            children: PropTypes.node
+        };
+
+        return (
+            <section className={classnames(storyStyles.storyWrapper)}>
+                <Parent>
+                    {(state, setState) => (
+                        <Input
+                            readOnly
+                            value={state}
+                            onChange={e => {
+                                setState(e.target.value);
+                            }}
+                        />
+                    )}
+                </Parent>
+            </section>
+        );
+    });
