93b281fb759171e516c575b2afd237b67338ef4e
Vindhya <vindhya.raviraj@gmail.com>
2020-10-09 09:27:42 -0400

    MOON-156: Migrates MenuItem to Typescript and Adds Image Support for MenuItem and ListItem (#182)
    * MOON-156: Renames ListItem index.js to index.ts
    
    * MOON-156: Adds tabIndex prop to ListItem component
    
    * MOON-156: Migrates MenuItem component to typescript
    
    * MOON-156: Adds image to MenuItem
    
    * Update src/components/Menu/MenuItem.scss
    
    Co-authored-by: Félix Vallé <31360150+ffffffelix@users.noreply.github.com>
    
    * Update src/components/ListItem/ListItem.scss
    
    Co-authored-by: Félix Vallé <31360150+ffffffelix@users.noreply.github.com>
    
    * MOON-156: Allows images of varying ratios to be centered into square
    
    * MOON-156: Moves image from MenuItem to ListItem; same API
    
    * MOON-156: Renames index.js for Menu to index.ts
    
    * MOON-156: Removes ability to use iconStart and image together in ListItem
    
    * Update src/components/ListItem/ListItem.stories.jsx
    
    Co-authored-by: Félix Vallé <31360150+ffffffelix@users.noreply.github.com>
    
    * MOON-156: Small changes from PR comments
    
    Co-authored-by: Félix Vallé <31360150+ffffffelix@users.noreply.github.com>

--- src/components/ListItem/ListItem.tsx
+++ src/components/ListItem/ListItem.tsx
@@ -3,6 +3,12 @@ import classnames from 'clsx';
 import './ListItem.scss';
 import {Typography} from '~/components/Typography';
 
+type TListItemImageSize = 'small' | 'big';
+enum ListItemImageSize {
+    SMALL = 'small',
+    BIG = 'big'
+}
+
 interface IListItemProps {
     /**
      * Additional classname
@@ -20,14 +26,28 @@ interface IListItemProps {
     isHtml?: boolean,
 
     /**
-     * Icon display before the label
+     * Icon display before the label. Cannot be used in conjunction with the image property.
      */
     iconStart?: React.ReactElement,
 
     /**
      * Icon display at the end of ListItem
      */
-    iconEnd?: React.ReactElement
+    iconEnd?: React.ReactElement,
+    /**
+     * Tab index for the element
+     */
+    tabIndex?: number
+
+    /**
+     * Optional image to display to describe the menu item. Cannot be used in conjunction with the iconStart property.
+     */
+    image?: HTMLImageElement,
+
+    /**
+     * If there's an image, it should be this size
+     */
+    imageSize?: TListItemImageSize,
 }
 
 export const ListItem: React.FC<IListItemProps> = ({
@@ -35,6 +55,9 @@ export const ListItem: React.FC<IListItemProps> = ({
     isHtml = false,
     iconStart = null,
     iconEnd = null,
+    tabIndex,
+    image,
+    imageSize,
     className = '',
     ...props
 }) => {
@@ -46,12 +69,19 @@ export const ListItem: React.FC<IListItemProps> = ({
     );
 
     return (
-        <li className={classnames(cssListItem)}
+        <li
+            className={classnames(cssListItem)}
+            tabIndex={tabIndex}
             {...props}
         >
             {
-            iconStart &&
-                <iconStart.type {...iconStart.props} size="small" className={classnames('moonstone-listItem_iconStart')}/>
+                iconStart && !image &&
+                <div className="moonstone-listItem_iconStart"><iconStart.type {...iconStart.props} size="small"/></div>
+            }
+
+            {
+                image && !iconStart &&
+                <figure className={`moonstone-listItem-image_${imageSize} flexRow`}>{image}</figure>
             }
 
             <Typography
@@ -65,8 +95,8 @@ export const ListItem: React.FC<IListItemProps> = ({
             </Typography>
 
             {
-            iconEnd &&
-                <iconEnd.type {...iconEnd.props} size="small" className={classnames('moonstone-listItem_iconEnd')}/>
+                iconEnd &&
+                <div className="moonstone-listItem_iconStart"><iconEnd.type {...iconEnd.props} size="small"/></div>
             }
         </li>
     );
---
+++ src/components/Menu/MenuItem.tsx
@@ -0,0 +1,121 @@
+import React from 'react';
+import {ListItem} from '~/components/ListItem';
+import classnames from 'clsx';
+import './MenuItem.scss';
+
+type TMenuItemVariant = 'default' | 'title';
+enum MenuItemVariant {
+    DEFAULT = 'default',
+    TITLE = 'title'
+}
+
+type TMenuItemImageSize = 'small' | 'big';
+enum MenuItemImageSize {
+    SMALL = 'small',
+    BIG = 'big'
+}
+
+interface IMenuItemProps {
+    /**
+     * Additional classname
+     */
+    className?: string,
+
+    /**
+     * Is item being hovered
+     */
+    isHover?: boolean,
+
+    /**
+     * Is item selected
+     */
+    isSelected?: boolean,
+
+    /**
+     * Is item disabled
+     */
+    isDisabled?: boolean,
+
+    /**
+     * MenuItem label
+     */
+    label: React.ReactNode,
+
+    /**
+     * Does the label contain HTML markup
+     */
+    isHtml?: boolean,
+
+    /**
+     * Icon display before the label
+     */
+    iconStart?: React.ReactElement,
+
+    /**
+     * Icon display at the end of MenuItem
+     */
+    iconEnd?: React.ReactElement,
+
+    /**
+     * MenuItem variants
+     */
+    variant?: TMenuItemVariant,
+
+    /**
+     * Optional image to display to describe the menu item
+     */
+    image?: HTMLImageElement,
+
+    /**
+     * If there's an image, it should be this size
+     */
+    imageSize?: TMenuItemImageSize,
+
+    /**
+     * Function triggered on clicking the item
+     */
+    onClick?: (event: React.MouseEvent) => void,
+
+    /**
+     * Function triggered when the mouse pointer hovering the item
+     */
+    onMouseEnter?: (event: React.MouseEvent) => void,
+
+    /**
+     * Function triggered when the mouse pointer move off the item
+     */
+    onMouseLeave?: (event: React.MouseEvent) => void
+};
+
+export const MenuItem: React.FC<IMenuItemProps> = ({
+    variant = MenuItemVariant.DEFAULT,
+    isHover,
+    isSelected,
+    isDisabled = false,
+    iconStart = null,
+    iconEnd = null,
+    image,
+    imageSize,
+    className = '',
+    ...props
+}) => (
+    <ListItem
+        tabIndex={isDisabled || variant === MenuItemVariant.TITLE || isSelected ? null : 0}
+        aria-disabled={isDisabled}
+        className={classnames(
+            'moonstone-menuItem',
+            {
+                'moonstone-hover': isHover,
+                'moonstone-selected': isSelected,
+                'moonstone-disabled': isDisabled,
+                'moonstone-title': variant === MenuItemVariant.TITLE
+            },
+            image && 'moonstone-menuItem-image'
+        )}
+        image={image}
+        imageSize={imageSize}
+        {...props}
+    />
+);
+
+MenuItem.displayName = 'MenuItem';
