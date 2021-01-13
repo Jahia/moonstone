1af4a0b9d0ac7555fa64f1206bc666e4ddc3fbf4
Vindhya <vindhya.raviraj@gmail.com>
2021-01-13 09:24:17 -0500

    MOON-233: Fix for component description not appearing in Docs tab (#219)
    * MOON-233: Add menu item to menu subcomponents for docs
    
    * MOON-233: Update storybook
    
    * MOON-233: Extract notes parameter in stories to description section of docs

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -19,4 +19,12 @@ addDecorator(story => {
 
 export const parameters = {
     layout: 'fullscreen',
+    docs: {
+        extractComponentDescription: (component, { notes }) => {
+            if (notes) {
+                return typeof notes === 'string' ? notes : notes.markdown || notes.text;
+            }
+            return null;
+        },
+    }
 };
