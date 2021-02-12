c5eabec5382d76cce866135050256ba4cd1a7c8c
Vindhya <vindhya.raviraj@gmail.com>
2021-02-11 19:14:45 -0500

    TECH-342: Migrate ResizableBox to Typescript (#226)
    * TECH-342: WIP ResizableBox TS migration
    
    * TECH-342: Migrate ResizableBox to typescript
    
    * TECH-342: Remove unnecessary type assignment
    
    * TECH-342: Add role prop to ResizableBox

---
+++ src/components/ResizableBox/ResizableBox.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import {Resizable} from 're-resizable';
+import classnames from 'clsx';
+import './ResizableBox.scss';
+import HandleResize from '~/icons/HandleResize';
+import {zones, EnableZones, ResizableBoxProps} from './ResizableBox.types';
+
+export const ResizableBox: React.FC<ResizableBoxProps> = ({
+    enable = ['right'],
+    minWidth = 50,
+    maxWidth = 200,
+    defaultSize = {width: '100%', height: 'auto'},
+    className = '',
+    size,
+    children = null,
+    onResizeStart,
+    onResizing,
+    onResizeStop,
+    ...props
+}) => {
+    const enableZones: EnableZones = {};
+
+    zones.forEach(zone => {
+        enableZones[zone] = enable.indexOf(zone as any) > -1;
+    });
+
+    return (
+        <Resizable
+            role="resizable-panel"
+            enable={enableZones}
+            minWidth={minWidth}
+            maxWidth={maxWidth}
+            size={size}
+            defaultSize={defaultSize}
+            handleClasses={
+                {
+                    right: classnames('moonstone-resizable_handle'),
+                    left: classnames('moonstone-resizable_handle')
+                }
+            }
+            handleComponent={
+                {
+                    right: <HandleResize className={classnames('moonstone-resizable_handle_icon')} size="big"/>,
+                    left: <HandleResize className={classnames('moonstone-resizable_handle_icon')} size="big"/>
+                }
+            }
+            className={classnames(className)}
+            onResize={onResizing}
+            onResizeStart={onResizeStart}
+            onResizeStop={onResizeStop}
+            {...props}
+        >
+            {children}
+        </Resizable>
+    );
+};
+
+ResizableBox.displayName = 'ResizableBox';
--- src/components/ResizableBox/ResizableBox.types.ts
+++ src/components/ResizableBox/ResizableBox.types.ts
@@ -1,22 +1,30 @@
 import * as React from 'react';
+import {Size} from 're-resizable';
 
-export type ResizableBoxEnable = "right";
+export type EnableZones = {
+    // top?: boolean;
+    right?: boolean;
+    // bottom?: boolean;
+    // left?: boolean;
+    // topRight?: boolean;
+    // bottomRight?: boolean;
+    // bottomLeft?: boolean;
+    // topLeft?: boolean;
+};
+
+// WIP
+// const zones = ['top', 'right', 'bottom', 'left', 'topRight', 'bottomRight', 'bottomLeft', 'topLeft'];
+export const zones: Zones[] = ['right'];
+
+export type Zones = keyof EnableZones;
+
+export type ResizableBoxEnable = 'right';
 
 export type ResizableBoxMinWidth = string | number;
 
 export type ResizableBoxMaxWidth = string | number;
 
-export interface ResizableBoxDefaultSize {
-    width?: string | number;
-    height?: string | number;
-}
-
-export interface ResizableBoxSize {
-    width?: string | number;
-    height?: string | number;
-}
-
-export interface ResizableBoxProps {
+export type ResizableBoxProps = {
     /**
      * Content of the component
      */
@@ -36,15 +44,19 @@ export interface ResizableBoxProps {
     /**
      * Set the default size
      */
-    defaultSize?: ResizableBoxDefaultSize;
+    defaultSize?: Size;
     /**
      * Manage the size
      */
-    size?: ResizableBoxSize;
+    size?: Size;
     /**
      * Additional classname
      */
     className?: string;
+    /**
+     * Role
+     */
+    role?: string;
     /**
      * Function triggered when the resize begins
      * @param {object} e - Mouse event
@@ -66,6 +78,4 @@ export interface ResizableBoxProps {
      */
     onResizeStop?: (...args: any[])=>any;
 }
-
-export const ResizableBox: React.SFC<ResizableBoxProps>;
 
