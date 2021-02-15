21c2b88b8ad89ae769016913c0922db3ae7ba30f
Félix Vallé <fvalle@jahia.com>
2021-02-15 10:44:21 +0100

    MOON-236: Manage header's application with LayoutModule

--- src/layouts/module/LayoutModule.spec.jsx
+++ src/layouts/module/LayoutModule.spec.jsx
@@ -8,14 +8,24 @@ describe('LayoutModule', () => {
         expect(screen.getByText('test-navigation')).toBeInTheDocument();
     });
 
+    it('should display the loader and not the content when LayoutModule is loading', () => {
+        render(<LayoutModule isLoading content="my content"/>);
+        expect(screen.getByRole('status')).toBeInTheDocument();
+        expect(screen.queryByText('my content')).not.toBeInTheDocument();
+    });
+
     it('should display a specific HTML markup', () => {
         const {container} = render(<LayoutModule component="section"/>);
         expect(container.querySelector('section')).toBeInTheDocument();
     });
 
-    it('should display the loader and not the content when LayoutModule is loading', () => {
-        render(<LayoutModule isLoading content="my content"/>);
-        expect(screen.getByRole('status')).toBeInTheDocument();
-        expect(screen.queryByText('my content')).not.toBeInTheDocument();
+    it('should have the class `moonstone-layoutModule_content_centered` when `isCentered` set to true', () => {
+        const {container} = render(<LayoutModule isCentered content="my content"/>);
+        expect(container.querySelector('.moonstone-layoutModule_content_centered')).toBeInTheDocument();
+    });
+
+    it('should have the class `moonstone-layoutModule_content_withNoPadding` when `hasPadding` set to false', () => {
+        const {container} = render(<LayoutModule hasPadding={false} content="my content"/>);
+        expect(container.querySelector('.moonstone-layoutModule_content_withNoPadding')).toBeInTheDocument();
     });
 });
--- src/layouts/module/LayoutModule.tsx
+++ src/layouts/module/LayoutModule.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import clsx from 'clsx';
 import {LayoutModuleProps} from './LayoutModule.types';
+import './LayoutModule.scss';
 import {Loader} from '~/components/Loader';
 
 export const LayoutModule: React.FC<LayoutModuleProps> = ({
     navigation = null,
+    header = null,
     content = null,
+    component = 'main',
     isLoading = false,
-    component = 'main'
+    isCentered = false,
+    hasPadding = true
 }) => {
 
     const classNameProps = clsx(
         'flexFluid',
+        'moonstone-layoutModule_content',
+        {'moonstone-layoutModule_content_withNoPadding': !hasPadding},
         isLoading ? ['flexCol_center', 'alignCenter'] : 'flexCol'
     );
 
@@ -23,8 +29,19 @@ export const LayoutModule: React.FC<LayoutModuleProps> = ({
             {
                 React.createElement(
                     component,
-                    {className: clsx(classNameProps), 'role-busy': isLoading ? 'true' : undefined},
-                    isLoading ? <Loader size="big"/> : content
+                    {
+                        className: clsx('moonstone-layoutModule_main', 'flexCol', 'flexFluid'),
+                    },
+                    (
+                        <>
+                            {header}
+                            <div className={clsx(classNameProps)} role-busy={isLoading ? 'true' : undefined}>
+                                <div className={clsx({'moonstone-layoutModule_content_centered': isCentered})}>
+                                    {isLoading ? <Loader size="big"/> : content}
+                                </div>
+                            </div>
+                        </>
+                    )
                 )
             }
         </>
