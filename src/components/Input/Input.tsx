278b7ae687d321c43427cb76e37898286275e1b8
Vindhya Raviraj <vindhya.raviraj@gmail.com>
2021-03-10 18:04:54 -0500

    TECH-349: Input type organization cleanup

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,76 +1,8 @@
 import React from 'react';
 import clsx from 'clsx';
-
 import {Cancel, Search} from '~/icons';
 import './Input.scss';
-
-type TInputVariant = 'text' | 'search';
-export enum InputVariant {
-    Text = 'text',
-    Search = 'search'
-}
-
-type TInputSize = 'default' | 'big';
-
-export enum InputSize {
-    Default = 'default',
-    Big = 'big'
-}
-
-interface InputProps {
-    /**
-     * Variant of the input to use
-     */
-    variant?: TInputVariant;
-    /**
-     * Value to exist in the input field
-     */
-    value?: string;
-    /**
-     * ID of the input
-     */
-    id?: string,
-    /**
-     * Initial placeholder text to appear in the input field
-     */
-    placeholder?: string;
-    /**
-     * Whether the input should be disabled
-     */
-    isDisabled?: boolean;
-    /**
-     * Whether the input should be read-only. It's still submittable.
-     */
-    isReadOnly?: boolean;
-    /**
-     * Any additional custom classes to apply to the component
-     */
-    className?: string;
-    /**
-     * Whether the size of the input should be default or big
-     */
-    size?: TInputSize;
-    /**
-     * Which icon to use at the beginning of the input
-     */
-    icon?: React.ReactElement;
-    /**
-     * Function - when passed in, the Cancel icon appears at the end of the input and its click event is passed back when the Cancel icon is clicked
-     */
-    onClear?: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
-    /**
-     * Function triggered on change of the input value
-     */
-    onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
-    /**
-     * Function triggered on blur of the input (i.e., focussing away from the input)
-     */
-    onBlur?: (event: React.FocusEvent<HTMLInputElement>) => void;
-    /**
-     * Function triggered on focus of the input
-     */
-    onFocus?: (event: React.FocusEvent<HTMLInputElement>) => void;
-}
+import {InputProps, InputSizes, InputVariants} from './Input.types';
 
 export const Input: React.FC<InputProps> =
     ({
@@ -81,7 +13,7 @@ export const Input: React.FC<InputProps> =
         isDisabled = false,
         isReadOnly = false,
         className,
-        size = InputSize.Default,
+        size = InputSizes.Default,
         icon,
         onClear,
         onChange,
@@ -91,7 +23,7 @@ export const Input: React.FC<InputProps> =
     }) => {
     const classNameProps = clsx(
         'moonstone-input',
-        {'moonstone-size_big': size === InputSize.Big},
+        {'moonstone-size_big': size === InputSizes.Big},
         {'moonstone-disabled': isDisabled},
         className
     );
@@ -104,7 +36,7 @@ export const Input: React.FC<InputProps> =
                 className={
                     clsx(
                         'moonstone-input-element',
-                        {'start-icon-padding': icon || variant === InputVariant.Search},
+                        {'start-icon-padding': icon || variant === InputVariants.Search},
                         {'end-icon-padding': onClear}
                     )
                 }
@@ -119,7 +51,7 @@ export const Input: React.FC<InputProps> =
                 onFocus={onFocus}
                 {...props}
             />
-            {(icon || variant === InputVariant.Search) && (
+            {(icon || variant === InputVariants.Search) && (
                 <div
                     className={clsx(
                         'start-icon-wrap',
@@ -129,7 +61,7 @@ export const Input: React.FC<InputProps> =
                         {'icon_input-empty': inputEmpty}
                     )}
                 >
-                    {variant === InputVariant.Search
+                    {variant === InputVariants.Search
                         ? <Search focusable="false" />
                         : <icon.type {...icon.props} focusable="false"/>
                     }
