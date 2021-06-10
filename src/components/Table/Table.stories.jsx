f92e29a636a8ecf967466796256ffc2fa8a1eed9
Vindhya <vindhya.raviraj@gmail.com>
2021-06-10 10:07:13 -0400

    MOON-196: Example for useFlexLayout react-table plugin to manage column widths of table (#252)

--- src/components/Table/Table.stories.jsx
+++ src/components/Table/Table.stories.jsx
@@ -1,9 +1,11 @@
-// This file has been written in jsx instead of tsx due to issues using
-// typescript with react-table. When react-table v8 is released in 2021,
-// these issues should be resolved.
+/*
+    This file has been written in jsx instead of tsx due to issues using
+    Typescript with react-table. When react-table v8 is released in 2021,
+    these issues should be resolved.
+*/
 
 import React, {useEffect, useState} from 'react';
-import {useExpanded, useRowSelect, useSortBy, useTable} from 'react-table';
+import {useExpanded, useFlexLayout, useRowSelect, useSortBy, useTable} from 'react-table';
 import storyStyles from '~/__storybook__/storybook.module.scss';
 
 import {
@@ -476,6 +478,84 @@ export const StructuredView = () => {
 };
 
 StructuredView.storyName = 'Structured View with React-Table';
+
+export const ColumnWidthReactTable = () => {
+    const data = React.useMemo(() => tableDataFlat, []);
+    const columns = React.useMemo(() => [
+        /*
+            Use the width property in these column definitions to specify column widths.
+            A column without a width property, such as the column definition immediately
+            below this comment, will expand to fit any remaining space.
+                "`width` is used as both the flex-basis and flex-grow. This means that it
+                essentially acts as both the minimum width and flex-ratio of the column."
+                - https://react-table.tanstack.com/docs/api/useFlexLayout
+        */
+        {Header: 'Name', id: 'name', accessor: row => row.name.value},
+        {Header: 'Status', accessor: 'status', width: 35},
+        {Header: 'Content Type', accessor: 'type', width: 50},
+        {Header: 'Created By', accessor: 'createdBy', width: 30},
+        {Header: 'Last Modified On', accessor: 'lastModifiedOn', width: 50}
+    ], []);
+
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow
+    } = useTable(
+        {
+            data,
+            columns
+        },
+        // Use the useFlexLayout plugin to render the table elements with flexbox
+        // (instead of with table element widths which are calculated by the browser)
+        useFlexLayout
+    );
+
+    return (
+        <Table {...getTableProps()}>
+            <TableHead>
+                {headerGroups.map(headerGroup => (
+                    // A key is included in headerGroup.getHeaderGroupProps
+                    // eslint-disable-next-line react/jsx-key
+                    <TableRow {...headerGroup.getHeaderGroupProps()}>
+                        {headerGroup.headers.map(column => (
+                            // A key is included in column.getHeaderProps
+                            // eslint-disable-next-line react/jsx-key
+                            <TableHeadCell {...column.getHeaderProps()}>
+                                {column.render('Header')}
+                            </TableHeadCell>
+                        ))}
+                    </TableRow>
+                ))}
+            </TableHead>
+            <TableBody {...getTableBodyProps()}>
+                {rows.map(row => {
+                    prepareRow(row);
+                    return (
+                        // A key is included in row.getRowProps
+                        // eslint-disable-next-line react/jsx-key
+                        <TableRow {...row.getRowProps()}>
+                            {row.cells.map(cell => (
+                                // A key is included in cell.getCellProps
+                                // eslint-disable-next-line react/jsx-key
+                                <TableBodyCell
+                                    {...cell.getCellProps()}
+                                    iconStart={cell.column.id === 'name' && row.original.name.icon}
+                                >
+                                    {cell.render('Cell')}
+                                </TableBodyCell>
+                            ))}
+                        </TableRow>
+                    );
+                })}
+            </TableBody>
+        </Table>
+    );
+};
+
+ColumnWidthReactTable.storyName = 'Column Width Management with React-Table';
 
 // Export const KitchenSink = () => {};
 // KitchenSink.storyName = 'Everything and the Kitchen Sink with React-Table';
