853ec4ce10f2d5d43f7acce4c30a94a1c8bc4c4d
Félix Vallé <fvalle@jahia.com>
2021-07-26 16:27:56 +0200

    MOON-309: Add big and default size for the Checkbox component

--- src/components/Checkbox/ControlledCheckbox.tsx
+++ src/components/Checkbox/ControlledCheckbox.tsx
@@ -3,6 +3,7 @@ import clsx from 'clsx';
 import {useCheckbox} from '@react-aria/checkbox';
 import {useToggleState} from '@react-stately/toggle';
 import {useFocusRing} from '@react-aria/focus';
+import {capitalize} from '~/utils/helpers';
 
 import {ControlledCheckboxProps} from './ControlledCheckbox.types';
 import './Checkbox.scss';
@@ -10,7 +11,8 @@ import './Checkbox.scss';
 export const ControlledCheckbox: React.FC<ControlledCheckboxProps> = props => {
     const {
         className,
-        isIndeterminate = false
+        isIndeterminate = false,
+        size = 'default'
     } = props;
     const inputRef = React.useRef<HTMLInputElement>(null);
     const {inputProps} = useCheckbox(props, useToggleState(props), inputRef);
@@ -19,12 +21,12 @@ export const ControlledCheckbox: React.FC<ControlledCheckboxProps> = props => {
     return (
         <div className={clsx('moonstone-checkbox', className)}>
             <input
-                className={clsx('moonstone-checkbox_input', {'hasFocus': isFocusVisible})}
+                className={clsx('moonstone-checkbox_input', `moonstone-checkbox_size${capitalize(size)}`, {'hasFocus': isFocusVisible})}
                 ref={inputRef}
                 {...inputProps}
                 {...focusProps}
             />
-            <svg className={clsx('moonstone-checkbox_icon')} viewBox="0 0 21 21">
+            <svg className={clsx('moonstone-checkbox_icon', `moonstone-checkbox_size${capitalize(size)}`)} viewBox={`0 0 21 21`}>
                 { isIndeterminate
                     ? <path d="M4.5 10.5L16.5 10.5" strokeLinecap="round"/>
                     : <path d="M5 10.75L8.5 14.25L16 6" strokeLinecap="round"/>
