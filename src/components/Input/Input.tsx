dfc2a5913972c78a16b0cd456dca710782553313
Thomas Draier <tdraier@jahia.com>
2021-12-10 10:15:56 +0100

    Update src/components/Input/Input.tsx

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -3,9 +3,9 @@ import {InputProps} from './Input.types';
 import {UncontrolledInput} from './UncontrolledInput';
 import {ControlledInput} from './ControlledInput';
 
-export const Input: React.FC<InputProps> = ({defaultValue, value, ...props}) => {
+export const Input: React.FC<InputProps> = ({value, ...props}) => {
     if (typeof value === 'undefined') {
-        return <UncontrolledInput defaultValue={defaultValue} {...props}/>;
+        return <UncontrolledInput {...props}/>;
     }
 
     return <ControlledInput value={value} {...props}/>;
