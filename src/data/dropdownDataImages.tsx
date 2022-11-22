4f674217e2eaac3d742ba1f3a119c4ff01074059
carp@jahia.com <carp@jahia.com>
2022-11-22 18:44:29 +0100

    MOON-353: Fix images placeholder.

--- src/data/dropdownDataImages.tsx
+++ src/data/dropdownDataImages.tsx
@@ -7,17 +7,17 @@ export const dropdownDataImages = [
             {
                 label: 'option 1',
                 value: '1',
-                image: <img src="https://via.placeholder.com/500x500?text=DropdownOptionImage"/>
+                image: <img src="https://via.placeholder.com/500?text=DropdownOptionImage"/>
             },
             {
                 label: 'option 2',
                 value: '2',
-                image: <img src="https://via.placeholder.com/500x500?text=DropdownOptionImage"/>
+                image: <img src="https://via.placeholder.com/500?text=DropdownOptionImage"/>
             },
             {
                 label: 'option 3 with very long long label label label label label label label label',
                 value: '3',
-                image: <img src="https://via.placeholder.com/500x500?text=DropdownOptionImage"/>
+                image: <img src="https://via.placeholder.com/500?text=DropdownOptionImage"/>
             }
         ]
     },
@@ -28,43 +28,43 @@ export const dropdownDataImages = [
                 label: 'option 4',
                 value: '4',
                 isDisabled: true,
-                image: <img src="https://via.placeholder.com/500x500?text=DropdownOptionImage"/>
+                image: <img src="https://via.placeholder.com/500?text=DropdownOptionImage"/>
             },
             {
                 label: 'option 5',
                 value: '5',
-                image: <img src="https://via.placeholder.com/500x500?text=DropdownOptionImage"/>,
+                image: <img src="https://via.placeholder.com/500?text=DropdownOptionImage"/>,
                 isDisabled: true
             },
             {
                 label: 'option 6',
                 value: '6',
-                image: <img src="https://via.placeholder.com/500x500?text=DropdownOptionImage"/>
+                image: <img src="https://via.placeholder.com/500?text=DropdownOptionImage"/>
             },
             {
                 label: 'option 7',
                 value: '7',
-                image: <img src="https://via.placeholder.com/500x500?text=DropdownOptionImage"/>
+                image: <img src="https://via.placeholder.com/500?text=DropdownOptionImage"/>
             },
             {
                 label: 'option 8',
                 value: '8',
-                image: <img src="https://via.placeholder.com/500x500?text=DropdownOptionImage"/>
+                image: <img src="https://via.placeholder.com/500?text=DropdownOptionImage"/>
             },
             {
                 label: 'option 9',
                 value: '9',
-                image: <img src="https://via.placeholder.com/500x500?text=DropdownOptionImage"/>
+                image: <img src="https://via.placeholder.com/500?text=DropdownOptionImage"/>
             },
             {
                 label: 'option 10',
                 value: '10',
-                image: <img src="https://via.placeholder.com/500x500?text=DropdownOptionImage"/>
+                image: <img src="https://via.placeholder.com/500?text=DropdownOptionImage"/>
             },
             {
                 label: 'option 11',
                 value: '11',
-                image: <img src="https://via.placeholder.com/500x500?text=DropdownOptionImage"/>
+                image: <img src="https://via.placeholder.com/500?text=DropdownOptionImage"/>
             }
         ]
     }
