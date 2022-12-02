b7287d22fdf9984b491aa597da6151e43885f2ec
Alex Karmanov <alexandrk_@hotmail.com>
2022-12-02 07:04:38 -0500

    BACKLOG-20304 Fix lint

--- src/components/MultipleLeftRightSelector/MultipleLeftRightSelector.stories.tsx
+++ src/components/MultipleLeftRightSelector/MultipleLeftRightSelector.stories.tsx
@@ -46,7 +46,7 @@ ReadOnly.args = {
 export const Controlled: ComponentStory<typeof MultipleLeftRightSelector> = args => {
     const [arrayValue, setArrayValue] = useState([]);
 
-    const options = [{value: '1', label: 'One'}, {value: '2', label: 'Two'}, {value: '3', label: 'Three'}, {value: '4', label: 'Four'}, {value: '5', label: 'Five'}]
+    const options = [{value: '1', label: 'One'}, {value: '2', label: 'Two'}, {value: '3', label: 'Three'}, {value: '4', label: 'Four'}, {value: '5', label: 'Five'}];
 
     return <MultipleLeftRightSelector options={options} arrayValue={arrayValue} onChange={setArrayValue}/>;
 };
