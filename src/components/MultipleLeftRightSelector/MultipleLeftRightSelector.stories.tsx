2d8300b8593b5875552c014d3d91d8eb4dbdb18c
Alex Karmanov <alexandrk_@hotmail.com>
2022-12-05 13:44:35 -0500

    BACKLOG-20304 Pass args

--- src/components/MultipleLeftRightSelector/MultipleLeftRightSelector.stories.tsx
+++ src/components/MultipleLeftRightSelector/MultipleLeftRightSelector.stories.tsx
@@ -48,5 +48,5 @@ export const Controlled: ComponentStory<typeof MultipleLeftRightSelector> = args
 
     const options = [{value: '1', label: 'One'}, {value: '2', label: 'Two'}, {value: '3', label: 'Three'}, {value: '4', label: 'Four'}, {value: '5', label: 'Five'}];
 
-    return <MultipleLeftRightSelector options={options} arrayValue={arrayValue} onChange={setArrayValue}/>;
+    return <MultipleLeftRightSelector {...args} options={options} arrayValue={arrayValue} onChange={setArrayValue}/>;
 };
