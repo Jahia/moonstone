9d1c038a89a0d364350487cbeb4334e7150ebfd3
Félix Vallé <fvalle@jahia.com>
2022-12-14 10:05:03 +0100

    MOON-361: Add new icons

---
+++ src/icons/components/Textarea.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import {SVGProps} from 'react';
+interface IconProps extends SVGProps<SVGSVGElement> {
+  size?: 'small' | 'default' | 'big';
+  color?: 'red' | 'yellow' | 'green' | 'blue' | 'deepBlue' | 'purple' | 'gray';
+  className?: string;
+}
+
+const SvgTextarea = ({
+    size = 'default',
+    className = '',
+    color,
+    ...otherProps
+}: IconProps) => {
+    const props = Object.assign(
+        {},
+        {
+            size,
+            className,
+            ...otherProps
+        }
+    );
+    const classNameColor = color ? ' moonstone-icon_' + color : '';
+    props.className =
+    className + ' moonstone-icon moonstone-icon_' + size + classNameColor;
+    return (
+        <svg
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="currentColor"
+      {...props}
+        >
+            <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M4.79999 8.8002C4.79999 12.773 7.98669 14.4002 11.2 14.4002V20.0002C11.2 20.442 11.5582 20.8002 12 20.8002C12.4418 20.8002 12.8 20.442 12.8 20.0002V14.4002V4.9602H14.4V20.0002C14.4 20.442 14.7582 20.8002 15.2 20.8002C15.6418 20.8002 16 20.442 16 20.0002V4.9602H18.4C18.8861 4.9602 19.28 4.56621 19.28 4.0802C19.28 3.59418 18.8861 3.2002 18.4 3.2002H15.2H12.8H12H11.2C7.98669 3.2002 4.79999 4.8274 4.79999 8.8002Z"
+      />
+        </svg>
+    );
+};
+
+export default SvgTextarea;
