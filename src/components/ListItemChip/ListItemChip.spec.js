c34a619608ea110a2f274c901ecee4ec7620b375
Félix Vallé <fvalle@jahia.com>
2023-12-07 12:27:35 +0100

    MOON-407: Add few tests

--- src/components/ListItemChip/ListItemChip.spec.js
+++ src/components/ListItemChip/ListItemChip.spec.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import {render, screen} from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import {ListItemChip} from './index';
 
 describe('ListItem', () => {
     it('should display label', () => {
         render(<ListItemChip label="Say my name"/>);
-        expect(screen.getByText('Say my name')).toBeTruthy();
+        expect(screen.getByText('Say my name')).toBeInTheDocument();
+    });
+
+    it('should add additional class names', () => {
+        const testClassName = 'hello';
+        render(<ListItemChip data-testid="moonstone-listItemChip" className={testClassName} label="Say my name"/>);
+        expect(screen.getByTestId('moonstone-listItemChip')).toHaveClass(testClassName);
+    });
+
+    it('should add additional attribute', () => {
+        render(<ListItemChip data-testid="moonstone-listItemChip" label="Say my name"/>);
+        expect(screen.getByTestId('moonstone-listItemChip')).toBeInTheDocument();
     });
 });
