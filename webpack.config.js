541b50c957686450462200e09f71dadb4a50a63b
bpapez <bpapez@jahia.com>
2024-06-07 09:46:10 +0200

    TECH-1270: Add SBOM creation

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,14 @@ const TerserJSPlugin = require('terser-webpack-plugin');
 const nodeExternals = require('webpack-node-externals');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const rules = require('./dist/rulesconfig-wp');
+const {CycloneDxWebpackPlugin} = require('@cyclonedx/webpack-plugin');
+
+/** @type {import('@cyclonedx/webpack-plugin').CycloneDxWebpackPluginOptions} */
+const cycloneDxWebpackPluginOptions = {
+    specVersion: '1.4',
+    rootComponentType: 'library',
+    outputLocation: './bom'
+};
 
 const productionPlugins =
     process.env.WEBPACK_MODE === 'production' ?
@@ -32,7 +40,8 @@ const ComponentsConfig = {
         new MiniCssExtractPlugin({
             filename: '[name].css',
             chunkFilename: '[id].css'
-        })
+        }),
+        new CycloneDxWebpackPlugin(cycloneDxWebpackPluginOptions)
     ]
 };
 
