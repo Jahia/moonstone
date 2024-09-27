9a0212ad83eea6e12411dcab3cee8392744456ef
Eevolee <eevee4224@gmail.com>
2024-09-27 11:38:55 +0200

    MOON-444: Add new icon Area

---
+++ src/icons/components/Area.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import {SVGProps} from 'react';
+interface IconProps extends SVGProps<SVGSVGElement> {
+  size?: 'small' | 'default' | 'big';
+  color?: 'red' | 'yellow' | 'green' | 'blue' | 'darkBlue' | 'purple' | 'gray';
+  className?: string;
+}
+
+const SvgArea = ({
+    size = 'default',
+    className = '',
+    color,
+    ...otherProps
+}: IconProps) => {
+    const props = Object.assign(
+        {},
+        {
+            size,
+            className,
+            ...otherProps
+        }
+    );
+    const classNameColor = color ? ' moonstone-icon_' + color : '';
+    props.className =
+    className + ' moonstone-icon moonstone-icon_' + size + classNameColor;
+    return (
+        <svg
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="currentColor"
+      {...props}
+        >
+            <path d="M2 4V11H22V4H2Z"/>
+            <path d="M2 13V20H8V13H2Z"/>
+            <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M10 13V20H22V13H10ZM12 18V15H20V18H12Z"
+      />
+        </svg>
+    );
+};
+
+export default SvgArea;
