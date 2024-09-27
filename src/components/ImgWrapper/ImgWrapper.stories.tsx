0e7354c259d8b3cf398740c1d4b0fdee9f1ec676
Eevolee <eevee4224@gmail.com>
2024-09-27 10:24:08 +0200

    MOON-384: Migrate ImgWrapper story to Typescript

---
+++ src/components/ImgWrapper/ImgWrapper.stories.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Story} from '@storybook/react';
+
+import {ImgWrapper} from './index';
+import {ImgWrapperProps} from './ImgWrapper.types';
+
+import markdownNotes from './ImgWrapper.md';
+
+export default {
+    title: 'Utilities/ImgWrapper',
+    component: ImgWrapper,
+    parameters: {
+        componentSubtitle: 'Image wrapper',
+        layout: 'centered',
+        notes: {markdown: markdownNotes}
+    }
+};
+
+const Template: Story<ImgWrapperProps> = args => <ImgWrapper {...args}/>;
+
+export const Default = Template.bind({});
+Default.args = {
+    src: "https://www.jahia.com/modules/jahiacom-templates/images/favicon/favicon.ico",
+    alt: "imgWrapper",
+    size: "default"
+};
