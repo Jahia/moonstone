3e2d64962b82f3ca174ef48a7138ab19f8d692ea
Alessia Bourgoin <151445105+Eevolee@users.noreply.github.com>
2024-10-09 14:48:58 +0200

    MOON-410: Add component Switch (#430)

---
+++ src/components/Switch/ControlledSwitch.tsx
@@ -0,0 +1,36 @@
+import React, {MutableRefObject, useRef} from 'react';
+import clsx from 'clsx';
+import type {ControlledSwitchProps} from './Switch.types';
+import './Switch.scss';
+
+const ControlledSwitchForwardRef: React.ForwardRefRenderFunction<HTMLDivElement, ControlledSwitchProps> = ({
+    className,
+    checked = false,
+    value,
+    onChange = () => undefined,
+    isDisabled,
+    ...other
+}, ref) => {
+    const inputRef: MutableRefObject<HTMLInputElement> = useRef();
+
+    return (
+        <div ref={ref} className={clsx('moonstone-switch', {'moonstone-switch_checked': checked, 'moonstone-switch_disabled': isDisabled}, className)}>
+            <input
+                {...other}
+                ref={inputRef}
+                className={clsx('moonstone-switch_input')}
+                type="checkbox"
+                value={value}
+                checked={checked}
+                disabled={isDisabled}
+                aria-checked={checked}
+                onChange={ev => (typeof onChange === 'function') && onChange(ev, value, inputRef.current?.checked)}
+            />
+            <span className={clsx('moonstone-switch_icon')}/>
+        </div>
+    );
+};
+
+export const ControlledSwitch = React.forwardRef(ControlledSwitchForwardRef);
+
+ControlledSwitch.displayName = 'ControlledSwitch';
---
+++ src/components/Switch/Switch.types.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+
+type BasicProps =
+Omit<React.ComponentPropsWithRef<'input'>, 'onChange' | 'value' | 'className'> & {
+    /**
+     * The value of the input element, used when submitting an HTML form
+     */
+    value?: string;
+
+    /**
+     * Additional classname
+     */
+    className?: string;
+
+    /**
+     * Whether the component should be disabled
+     */
+    isDisabled?: boolean;
+
+    /**
+     * Function triggered on change
+     */
+    onChange?: (event: React.ChangeEvent<HTMLInputElement>, value: string, checked: boolean) => void;
+}
+
+type ControlledProps = {
+    /**
+     * Whether the component should be checked (controlled)
+     */
+    checked?: boolean;
+}
+
+type UncontrolledProps = {
+    /**
+     * Whether the component should be checked - default value (uncontrolled)
+     */
+    defaultChecked?: boolean;
+}
+
+export type SwitchProps = BasicProps & Partial<ControlledProps> & Partial<UncontrolledProps>;
+export type ControlledSwitchProps = BasicProps & ControlledProps;
+export type UncontrolledSwitchProps = BasicProps & UncontrolledProps;
