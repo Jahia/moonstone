070e45da978cae5630b08f46b671cbb98bb46eed
Alessia Bourgoin <151445105+Eevolee@users.noreply.github.com>
2024-12-11 17:33:59 +0100

    MOON-246: Add Field component (#461)
    * MOON-246: Add Field, FieldBoolean & FieldSelector components
    
    * MOON-246: Fix lint errors & add markdown notes to stories
    
    * MOON-246: Fix cardSelector typing
    
    * MOON-246: Add cardSelector stories & fix text sizes
    
    * MOON-246: Improve design
    
    * MOON-246: Issue with yarn.lock
    
    * Revert "MOON-246: Issue with yarn.lock"
    
    This reverts commit 2222d76a9d88ed436a968816a11efb779cabc991.
    
    * Revert "MOON-246: Improve design"
    
    This reverts commit 71ccef0b418fd50cc141b9eb9e2902c40e9e94ab.
    
    * MOON-246: Improve design and truncate long label and helper
    
    * MOON-246: Fix lint error
    
    * MOON-246: Fix lint error (again)
    
    ---------
    
    Co-authored-by: Félix Vallé <fvalle@jahia.com>

---
+++ src/components/Field/Field.spec.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import {render, screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import {Field} from './index';
+import {FieldSelector} from './FieldSelector';
+import {Button, Chip} from '~/main';
+import {Add, Love} from '~/icons';
+
+describe('Field', () => {
+    it('should display additional class names', () => {
+        render(<Field data-testid="field" className="extra"><FieldSelector selector={<textarea placeholder="Input value"/>}/></Field>);
+        expect(screen.getByTestId('field')).toHaveClass('extra');
+    });
+
+    it('should display label', () => {
+        render(<Field label="Field label"/>);
+        expect(screen.queryByText('Field label')).toBeInTheDocument();
+    });
+
+    it('should display chips', () => {
+        render(<Field chips={<Chip label="Field chip"/>}/>);
+        expect(screen.queryByText('Field chip')).toBeInTheDocument();
+    });
+
+    it('should display helper', () => {
+        render(<Field helper="Field helper"/>);
+        expect(screen.queryByText('Field helper')).toBeInTheDocument();
+    });
+
+    it('should display children', () => {
+        render(<Field><FieldSelector selector={<textarea value="Input value"/>}/></Field>);
+        expect(screen.queryByText('Input value')).toBeInTheDocument();
+    });
+
+    it('should display multiple children', () => {
+        render(<Field><FieldSelector selector={<textarea value="Input value"/>}/><FieldSelector selector={<textarea value="Input value"/>}/></Field>);
+        expect(screen.getAllByText('Input value')).toHaveLength(2);
+    });
+
+    it('should display buttons', () => {
+        render(<Field buttons={<Button label="Click me"/>}/>);
+        expect(screen.queryByText('Click me')).toBeInTheDocument();
+    });
+
+    it('should display multiple buttons', () => {
+        render(<Field buttons={<><Button icon={<Add/>} label="Click me"/><Button icon={<Love/>} label="Click me"/></>}/>);
+        expect(screen.getAllByText('Click me')).toHaveLength(2);
+    });
+
+    it('should call onClick when button is clicked', async () => {
+        const onClick = jest.fn();
+        render(<Field buttons={<Button label="Click me" onClick={onClick}/>}/>);
+        await userEvent.click(screen.getByRole('button', {label: /Click me/i}));
+        expect(onClick).toHaveBeenCalled();
+    });
+
+    it('should display as error variant', () => {
+        render(<Field hasError data-testid="field"/>);
+        expect(screen.getByTestId('field')).toHaveClass('moonstone-field_error');
+    });
+
+    it('should display errorMessage', () => {
+        render(<Field hasError errorMessage="Field errorMessage"/>);
+        expect(screen.queryByText('Field errorMessage')).toBeInTheDocument();
+    });
+});
---
+++ src/components/Field/Field.types.ts
@@ -0,0 +1,48 @@
+import * as React from 'react';
+
+export type FieldProps = {
+    /**
+     * Field label
+     */
+    label: string;
+
+    /**
+     * Field's id
+     */
+    id: string;
+
+    /**
+     * Additional classname
+     */
+    className?: string;
+
+    /**
+     * Field chip(s)
+     */
+    chips?: React.ReactElement;
+
+    /**
+     * Field helper
+     */
+    helper?: string;
+
+    /**
+     * Define field selector(s)
+     */
+    children: React.ReactElement;
+
+    /**
+     * Field action(s)
+     */
+    buttons?: React.ReactElement;
+
+    /**
+     * Display as error
+     */
+    hasError?: boolean;
+
+    /**
+     * Error message
+     */
+    errorMessage?: string;
+};
