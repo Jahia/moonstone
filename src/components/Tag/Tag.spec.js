aff9651a6f0f510b072c2ad11a08931a0477c49a
Eevolee <eevee4224@gmail.com>
2025-01-28 17:26:41 +0100

    MOON-483: Improve Tag test coverage

--- src/components/Tag/Tag.spec.js
+++ src/components/Tag/Tag.spec.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import {render, screen} from '@testing-library/react';
-// Import userEvent from '@testing-library/user-event';
+import userEvent from '@testing-library/user-event';
 import {Tag} from './Tag';
 
 describe('Tag', () => {
     it('should display additional className', () => {
         render(<Tag data-testid="moonstone-paper" className="extra"/>);
         expect(screen.getByTestId('moonstone-paper')).toHaveClass('extra');
+    });
+
+    it('should call the onClick function', async () => {
+        const user = userEvent.setup();
+        const onClick = jest.fn();
+
+        render(<Tag label="tag" onClick={onClick}/>);
+        await user.click(screen.getByText('tag'));
+
+        expect(onClick).toHaveBeenCalled();
     });
 });
