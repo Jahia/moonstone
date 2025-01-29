bf4559fc92af4da7b160287333a656036c990f82
Eevolee <eevee4224@gmail.com>
2025-01-29 16:00:34 +0100

    MOON-483: Improve IconTextIcon test coverage

--- src/components/IconTextIcon/IconTextIcon.spec.js
+++ src/components/IconTextIcon/IconTextIcon.spec.js
@@ -9,6 +9,11 @@ describe('IconTextIcon', () => {
         expect(screen.getByTestId('moonstone-IconTextIcon')).toBeInTheDocument();
     });
 
+    it('should have extra classname', () => {
+        render(<IconTextIcon className="extra" data-testid="moonstone-IconTextIcon"/>);
+        expect(screen.getByTestId('moonstone-IconTextIcon')).toHaveClass('extra');
+    });
+
     it('should display iconStart', () => {
         render(<IconTextIcon iconStart={<Love data-testid="moonstone-IconTextIcon_iconStart"/>}/>);
         expect(screen.getByTestId('moonstone-IconTextIcon_iconStart')).toBeInTheDocument();
