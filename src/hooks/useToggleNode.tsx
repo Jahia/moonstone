5a354cea531bf35b67531e4657395a5425f65993
Eevolee <eevee4224@gmail.com>
2025-05-15 16:22:42 +0200

    add useToggleNode & tests

---
+++ src/hooks/useToggleNode.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+type onClickProp = (e: React.KeyboardEvent | React.MouseEvent) => void;
+type onToggleProp = (e: React.KeyboardEvent | React.MouseEvent) => void;
+
+type onToggleNodeProps = {
+    onClick?: onClickProp;
+    onKeyUp?: React.KeyboardEventHandler;
+    disabled?: boolean;
+    ariaSelected?: boolean;
+    tabIndex?: number;
+};
+
+export const onToggleNode = (
+    onToggle: onToggleProp,
+    onClick?: onClickProp,
+    isDisabled = false,
+    tabIndex = 0
+): onToggleNodeProps => {
+    const handleKeyUp = (e: React.KeyboardEvent) => {
+        if (e.code === 'Space') {
+            onToggle(e);
+        }
+
+        if (e.code === 'Enter') {
+            e.preventDefault();
+            if (onClick && typeof onClick === 'function' && !isDisabled) {
+                onClick(e);
+            } else {
+                onToggle(e);
+            }
+        }
+
+        if (e.code === 'ArrowLeft') {
+            onToggle(e);
+        }
+
+        if (e.code === 'ArrowRight') {
+            onToggle(e);
+        }
+    };
+
+    return {
+        onClick: isDisabled ? undefined : onClick,
+        onKeyUp: handleKeyUp,
+        disabled: isDisabled,
+        tabIndex: tabIndex
+    };
+};
