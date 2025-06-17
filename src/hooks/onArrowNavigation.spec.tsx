4d4530e425a70aa50b569ea2614366bc222e7e9d
Eevolee <eevee4224@gmail.com>
2025-06-17 12:18:04 +0200

    add direction prop on hook to handle both vertical & horizontal navigation

--- src/hooks/onArrowNavigation.spec.tsx
+++ src/hooks/onArrowNavigation.spec.tsx
@@ -3,16 +3,18 @@ import {onArrowNavigation} from './onArrowNavigation';
 import {render, screen} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
+type TestMenuProps = {readonly direction?: 'vertical' | 'horizontal' | 'both'};
+
 describe('onArrowNavigation', () => {
-    let TestMenu = () => {
+    const TestMenu = ({direction}: TestMenuProps) => {
         const refs = [useRef(null), useRef(null)];
 
         return (
             <div
                 data-testid="test-menu"
             >
-                <div ref={refs[0]} {...onArrowNavigation(refs[0])}>Item 1</div>
-                <div ref={refs[1]} {...onArrowNavigation(refs[1])}>Item 2</div>
+                <div ref={refs[0]} {...onArrowNavigation({ref: refs[0], direction: direction, tabIndex: 3})}>Item 1</div>
+                <div ref={refs[1]} {...onArrowNavigation({ref: refs[1], direction: direction, tabIndex: 3})}>Item 2</div>
             </div>
         );
     };
@@ -25,6 +27,11 @@ describe('onArrowNavigation', () => {
         expect(screen.queryByText('Item 2')).toHaveFocus();
     });
 
+    it('should have the right tabIndex', () => {
+        render(<TestMenu/>);
+        expect(screen.queryByText('Item 2')).toHaveAttribute('tabindex', '3');
+    });
+
     it('should focus previous sibling when arrowUp is pressed', async () => {
         const user = userEvent.setup();
         render(<TestMenu/>);
@@ -34,23 +41,40 @@ describe('onArrowNavigation', () => {
         expect(screen.queryByText('Item 1')).toHaveFocus();
     });
 
-    TestMenu = () => {
+    it('should focus next sibling when arrowRight is pressed', async () => {
+        const user = userEvent.setup();
+        render(<TestMenu direction="horizontal"/>);
+        await user.keyboard('[Tab]');
+        await user.keyboard('[ArrowRight]');
+        expect(screen.queryByText('Item 2')).toHaveFocus();
+    });
+
+    it('should focus previous sibling when arrowLeft is pressed', async () => {
+        const user = userEvent.setup();
+        render(<TestMenu direction="horizontal"/>);
+        await user.keyboard('[Tab]');
+        await user.keyboard('[ArrowRight]');
+        await user.keyboard('[ArrowLeft]');
+        expect(screen.queryByText('Item 1')).toHaveFocus();
+    });
+
+    const TestMenuWithUnfocusableSiblings = () => {
         const refs = [useRef(null), useRef(null)];
 
         return (
             <div
                 data-testid="test-menu"
             >
-                <div ref={refs[0]} {...onArrowNavigation(refs[0])}>Item 1</div>
+                <div ref={refs[0]} {...onArrowNavigation({ref: refs[0]})}>Item 1</div>
                 <div>Not an Item</div>
-                <div ref={refs[1]} {...onArrowNavigation(refs[1])}>Item 2</div>
+                <div ref={refs[1]} {...onArrowNavigation({ref: refs[1]})}>Item 2</div>
             </div>
         );
     };
 
     it('should focus next focusable sibling when arrowDown is pressed', async () => {
         const user = userEvent.setup();
-        render(<TestMenu/>);
+        render(<TestMenuWithUnfocusableSiblings/>);
         await user.keyboard('[Tab]');
         await user.keyboard('[ArrowDown]');
         expect(screen.queryByText('Item 2')).toHaveFocus();
@@ -58,10 +82,15 @@ describe('onArrowNavigation', () => {
 
     it('should focus previous focusable sibling when arrowUp is pressed', async () => {
         const user = userEvent.setup();
-        render(<TestMenu/>);
+        render(<TestMenuWithUnfocusableSiblings/>);
         await user.keyboard('[Tab]');
         await user.keyboard('[ArrowDown]');
         await user.keyboard('[ArrowUp]');
         expect(screen.queryByText('Item 1')).toHaveFocus();
+    });
+
+    it('should have the default tabIndex', () => {
+        render(<TestMenuWithUnfocusableSiblings/>);
+        expect(screen.queryByText('Item 2')).toHaveAttribute('tabindex', '0');
     });
 });
--- src/hooks/onArrowNavigation.tsx
+++ src/hooks/onArrowNavigation.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 
 type onArrowNavigationProps = {
     onKeyUp?: React.KeyboardEventHandler;
+    ref: React.RefObject<HTMLElement>;
+    direction?: 'vertical' | 'horizontal' | 'both';
     tabIndex?: number;
 };
 
-export const onArrowNavigation = (
-    ref: React.RefObject<HTMLElement>,
-    tabIndex = 0
-): onArrowNavigationProps => {
+export const onArrowNavigation = ({
+    ref,
+    direction = 'vertical',
+    tabIndex = 0} :
+    onArrowNavigationProps) => {
     const handleKeyUp = (e: React.KeyboardEvent) => {
         const element = ref.current;
 
@@ -24,14 +27,28 @@ export const onArrowNavigation = (
         const items = Array.from(container.querySelectorAll<HTMLElement>('[tabindex]')).filter(el => el.tabIndex >= 0);
         const currentIndex = items.indexOf(element);
 
-        if (e.key === 'ArrowDown' && currentIndex !== -1) {
-            e.preventDefault();
-            items[currentIndex + 1]?.focus();
+        if (direction === 'both' || direction === 'vertical') {
+            if (e.key === 'ArrowDown' && currentIndex !== -1) {
+                e.preventDefault();
+                items[currentIndex + 1]?.focus();
+            }
+
+            if (e.key === 'ArrowUp' && currentIndex !== -1) {
+                e.preventDefault();
+                items[currentIndex - 1]?.focus();
+            }
         }
 
-        if (e.key === 'ArrowUp' && currentIndex !== -1) {
-            e.preventDefault();
-            items[currentIndex - 1]?.focus();
+        if (direction === 'both' || direction === 'horizontal') {
+            if (e.key === 'ArrowRight' && currentIndex !== -1) {
+                e.preventDefault();
+                items[currentIndex + 1]?.focus();
+            }
+
+            if (e.key === 'ArrowLeft' && currentIndex !== -1) {
+                e.preventDefault();
+                items[currentIndex - 1]?.focus();
+            }
         }
     };
 
