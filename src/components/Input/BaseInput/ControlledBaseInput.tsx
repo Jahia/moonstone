9b5c606be557267526b94c0a285bc8f30a02d18a
Michael Krasnov <mihalKrasnov@gmail.com>
2025-07-17 08:36:40 -0400

    feat: fixes and improvements for CE integration

--- src/components/Input/BaseInput/ControlledBaseInput.tsx
+++ src/components/Input/BaseInput/ControlledBaseInput.tsx
@@ -18,6 +18,7 @@ export const ControlledBaseInput: React.FC<ControlledBaseInputProps> = ({
     variant = 'outlined',
     isShowClearButton,
     prefixComponents,
+    postfixComponents,
     onClick,
     onKeyPress,
     onKeyUp,
@@ -84,6 +85,7 @@ export const ControlledBaseInput: React.FC<ControlledBaseInputProps> = ({
                     onKeyUp={onKeyUp}
                     {...props}
                 />
+                {postfixComponents}
             </div>
             {onClear && isFilled && !isDisabled && !isReadOnly && (
                 <Button
