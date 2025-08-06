424f883de46390e19beaa5ef5694fff0ab0d3032
Eevolee <eevee4224@gmail.com>
2025-08-06 15:48:06 +0200

    add storybook beta tag

--- src/components/Modal/Modal.stories.tsx
+++ src/components/Modal/Modal.stories.tsx
@@ -10,7 +10,7 @@ import {useState} from 'react';
 const meta: Meta<typeof Modal> = {
     title: 'Components/Modal',
     component: Modal,
-    tags: ['new'],
+    tags: ['beta'],
 
     parameters: {
         layout: 'centered'
