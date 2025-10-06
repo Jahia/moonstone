b9cd8a0f793b474bd5941b92eadffb73b122ef71
Alessia Bourgoin <151445105+Eevolee@users.noreply.github.com>
2025-10-06 13:31:49 +0200

    feat: Add group titles to Dropdown search results (#1141)
    * add group titles to dropdown search results

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -6,7 +6,7 @@ import {MenuProps} from './Menu.types';
 import {SearchInput} from '~/components/Input';
 import {Typography} from '~/components/Typography';
 
-const getChildrenToFilter = (children: [React.ReactElement]) => {
+const getFlatChildren = (children: [React.ReactElement]) => {
     if (children[0].props['data-option-type'] === 'group') {
         return children.reduce((acc, curr) => {
             return [...acc, ...curr.props.children[2]];
@@ -14,6 +14,50 @@ const getChildrenToFilter = (children: [React.ReactElement]) => {
     }
 
     return children;
+};
+
+// Filtering function
+const getFilteredChildren = (child: React.ReactElement, inputValue: string) => {
+    if (child.props && (child.props.label || child.props.description)) {
+        const containsLabel = child.props.label ? child.props.label.toLowerCase().includes(inputValue.toLowerCase()) : false;
+        const containsDescription = child.props.description ? child.props.description.toLowerCase().includes(inputValue.toLowerCase()) : false;
+        return (containsLabel || containsDescription) && child.props.variant !== 'title';
+    }
+
+    return false;
+};
+
+const getFilteredGroups = (children: [React.ReactElement], inputValue: string) => {
+    // If group then filter each child by inputValue while keeping title & separator
+    if (children.length > 0 && children[0].props?.['data-option-type'] === 'group') {
+        return children.reduce((acc, curr) => {
+            const groupChildren = curr.props.children[2] || [];
+            const filteredChildren = groupChildren.filter((child: React.ReactElement) => getFilteredChildren(child, inputValue));
+
+            // If children match the inputValue then add group with only matching children to returning array
+            if (filteredChildren.length > 0) {
+                const filteredGroup = {
+                    ...curr,
+                    props: {
+                        ...curr.props,
+                        children: [
+                            // Separator
+                            curr.props.children[0],
+                            // Title
+                            curr.props.children[1],
+                            // Filtered children
+                            filteredChildren
+                        ]
+                    }
+                };
+                acc.push(filteredGroup);
+            }
+
+            return acc;
+        }, []);
+    }
+
+    return children.filter(child => getFilteredChildren(child, inputValue));
 };
 
 const defaultAnchorElOrigin = {
@@ -65,16 +109,7 @@ export const Menu: React.FC<MenuProps> = ({
     // UseEffect hook to filter the search results and determine whether to show the no search results text
     useEffect(() => {
         if (inputValue !== '' && Array.isArray(children)) {
-            const _childrenToFilter = getChildrenToFilter(children as [React.ReactElement]);
-            const _filtered = _childrenToFilter.filter((child: React.ReactElement) => {
-                if (child.props && (child.props.label || child.props.description)) {
-                    const containsLabel = child.props.label ? child.props.label.toLowerCase().includes(inputValue.toLowerCase()) : false;
-                    const containsDescription = child.props.description ? child.props.description.toLowerCase().includes(inputValue.toLowerCase()) : false;
-                    return (containsLabel || containsDescription) && child.props.variant !== 'title';
-                }
-
-                return false;
-            });
+            const _filtered = getFilteredGroups(children as [React.ReactElement], inputValue);
             setFilteredChildren(_filtered);
 
             if (_filtered.length === 0) {
@@ -94,7 +129,7 @@ export const Menu: React.FC<MenuProps> = ({
 
     let hasAutoSearch = hasSearch;
     if (typeof hasSearch === 'undefined') {
-        const flatChildren = (Array.isArray(children)) ? getChildrenToFilter(children as [React.ReactElement]) : children; // Check for grouped data
+        const flatChildren = (Array.isArray(children)) ? getFlatChildren(children as [React.ReactElement]) : children; // Check for grouped data
         hasAutoSearch = React.Children.count(flatChildren) > autoAddSearchLimit;
     }
 
