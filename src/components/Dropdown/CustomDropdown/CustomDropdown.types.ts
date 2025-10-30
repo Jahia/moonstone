8237d1601a3b42adce995ddefc8e073cc052076f
Alessia Bourgoin <151445105+Eevolee@users.noreply.github.com>
2025-10-30 15:23:46 +0100

    Update src/components/Dropdown/CustomDropdown/CustomDropdown.types.ts
    Co-authored-by: Félix Vallé <31360150+ffffffelix@users.noreply.github.com>

--- src/components/Dropdown/CustomDropdown/CustomDropdown.types.ts
+++ src/components/Dropdown/CustomDropdown/CustomDropdown.types.ts
@@ -1,5 +1,5 @@
-import {BaseDropdownProps} from '../Dropdown.types';
-import {ButtonSize} from '~/components/Button/Button.types';
+import type {BaseDropdownProps} from '../Dropdown.types';
+import type {ButtonSize} from '~/components/Button/Button.types';
 
 export type CustomDropdownProps = Omit<BaseDropdownProps,
 'onClear'| 'hasSearch' | 'autoAddSearchLimit'
