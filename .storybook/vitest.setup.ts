1e05b82a1cc839302a6bc12166c13c06ae6ad10b
Yacine Bouklif <ybouklif@jahia.com>
2025-11-04 09:26:21 +0100

    chore: add accessibility test with storybook UI

--- .storybook/vitest.setup.ts
+++ .storybook/vitest.setup.ts
@@ -1,6 +1,7 @@
 import { setProjectAnnotations } from '@storybook/react-vite';
+import * as a11yAnnotations from '@storybook/addon-a11y/preview';
 import * as projectAnnotations from './preview';
 
 // This is an important step to apply the right configuration when testing your stories.
 // More info at: https://storybook.js.org/docs/api/portable-stories/portable-stories-vitest#setprojectannotations
-setProjectAnnotations([projectAnnotations]);
+setProjectAnnotations([projectAnnotations, a11yAnnotations]);
