5f83f51380315c7e1940f9571fabb6b137385c1e
Félix Vallé <fvalle@jahia.com>
2025-11-19 15:38:07 +0100

    fix: error lint

--- src/components/Dropdown/CustomDropdown/CustomDropdown.stories.tsx
+++ src/components/Dropdown/CustomDropdown/CustomDropdown.stories.tsx
@@ -273,7 +273,7 @@ export const SortingDropdown = (args: CustomDropdownProps) => {
                             onValueChange(item);
                         }}
                     />
-            <Separator spacing='medium'/>
+            <Separator spacing="medium"/>
             <MenuItem label="Direction" variant="title"/>
             <Dropdown
                         variant="outlined"
