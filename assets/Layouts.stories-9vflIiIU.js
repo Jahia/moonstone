30334932bb59ed70aa0b3a769c8bdaa8e4625882
GauBen <GauBen@users.noreply.github.com>
2026-02-05 11:28:20 +0000

    Automated deployment - chore: display yarn instruction in pkg-pr-new comment - [skip ci] ec7c37aef5a71ed0e7886db91a73111521fec043

---
+++ assets/Layouts.stories-9vflIiIU.js
@@ -0,0 +1,77 @@
+import{j as e,r as h}from"./iframe-BeCTjTOk.js";import{t as w,a as y}from"./treeData-CfRt-Hcq.js";import{S as o}from"./File-BPAd5yJm.js";import{S as i}from"./index-BMQxb5Vg.js";import{c as t}from"./Subdirectory-D1wgoNPS.js";import{a as N,A as v}from"./AccordionItem-BtWBBRnG.js";import{l as C,F as D,a as L}from"./FakeComponents-CpaM6M4N.js";import{S as P}from"./Bug-CejVfnSG.js";import{S as A}from"./Love-CMlT9L_x.js";import{L as g}from"./LayoutApp-E-gXg5lO.js";import{L as I}from"./LayoutModule-I9WG3ayc.js";import{L as b}from"./LayoutContent-ClYx936j.js";import{P as d}from"./Paper-DzYBgpO1.js";import{H as q}from"./Header-D_0baC69.js";import{S as H,a as R}from"./SecondaryNavHeader-BRKu2prH.js";import{T as S}from"./TreeView-DvgCEtMp.js";import{P as k}from"./PrimaryNav-BbBNDoEG.js";import"./preload-helper-PPVm8Dsz.js";import"./SvgWrapper-Bz5YjAL0.js";import"./clsx-B-dksMZM.js";import"./ImgWrapper-Bp8PPddZ.js";import"./Visibility-DKkVAUgg.js";import"./Add-DD02N1wC.js";import"./Apps-DwWOrtXp.js";import"./ChevronLeft-BlamPhGu.js";import"./Setting-BCRAhOMw.js";import"./Build-vcNcoI3W.js";import"./Cancel-CxuB9Zi8.js";import"./CheckboxUnchecked-GuJ-sFib.js";import"./ChevronDoubleRight-C7eUuOtH.js";import"./ChevronDown-DUsfZPzW.js";import"./ChevronRight-BjPM4uW2.js";import"./Close-DQKfMWMl.js";import"./Cloud-CDztLNV4.js";import"./SiteWeb-BmyIW0kn.js";import"./Edit-Bp5tNulX.js";import"./FileBroken-CK8Iz9Yl.js";import"./FileImage-Ba0p62tG.js";import"./HandleDrag-0MVF__pQ.js";import"./HandleResize--uZp2Ib-.js";import"./RichText-DW01RYmN.js";import"./Home-BojZOW2L.js";import"./Image-MnQ9-C-V.js";import"./Language-BDjOVSos.js";import"./Lock-5wfSSRb0.js";import"./MoreVert-B0oUM1-e.js";import"./OpenInNew-B2mbS2qD.js";import"./Person-DiEXp8lE.js";import"./Workflow-Dde0vXqo.js";import"./RadioUnchecked-BKRAesCu.js";import"./Search-BI0j_py1.js";import"./Star-upqvcC8J.js";import"./ViewList-5HU-tKkC.js";import"./Warning-Dummdvet.js";import"./useAccessibleClick-Bdvd2I0f.js";import"./Typography-DFPlPLGV.js";import"./Loader-ClCMuRbS.js";import"./Separator-DvLC9WLx.js";import"./ResizableBox-DxHLOrEv.js";import"./index-D0SHVSMU.js";import"./index-D0ZFADPj.js";import"./onArrowNavigation-CltwoEwZ.js";import"./PrimaryNav.context-Q0MfoXRH.js";const F=[{id:"ROOT",label:"Root",iconStart:w("http://www.google.com/s2/favicons?domain=www.jahia.com"),isClosable:!1,children:[{id:"R1",label:"01 - praesent tristique magna sit amet purus gravida quis",iconStart:e.jsx(i,{}),iconEnd:e.jsx(t,{}),children:[{id:"02",label:"02 - phasellus vestibulum lorem sed risus ultricies tristique nulla",iconStart:e.jsx(i,{}),iconEnd:e.jsx(t,{}),children:[{id:"03",label:"03 - elit at imperdiet dui accumsan sit amet nulla",iconStart:e.jsx(i,{}),iconEnd:e.jsx(t,{}),children:[{id:"04",label:"04 - lorem ipsum",iconStart:e.jsx(i,{}),iconEnd:e.jsx(t,{}),children:[{id:"05",label:"05 - lectus urna duis convallis convallis",iconStart:e.jsx(i,{}),iconEnd:e.jsx(t,{}),children:[{id:"06",label:"06 - nisi porta lorem mollis aliquam",iconStart:e.jsx(i,{}),iconEnd:e.jsx(t,{}),children:[{id:"07",label:"07 - habitasse platea dictumst quisque sagittis purus sit amet volutpat consequat",iconStart:e.jsx(i,{}),iconEnd:e.jsx(t,{}),children:[{id:"08",label:"08 - enim diam vulputate ut pharetra sit amet aliquam id diam",iconStart:e.jsx(i,{}),iconEnd:e.jsx(t,{}),children:[{id:"09",label:"09 - imperdiet nulla malesuada pellentesque elit eget gravida cum sociis natoque",iconStart:e.jsx(i,{}),iconEnd:e.jsx(t,{}),children:[{id:"010",label:"010 - semper eget duis at tellus at urna condimentum mattis pellentesque id nibh tortor id aliquet lectus proin nibh",iconStart:e.jsx(i,{}),iconEnd:e.jsx(t,{}),children:[{id:"011",label:"011 - eget aliquet nibh praesent tristique magna sit amet purus gravida",iconStart:e.jsx(i,{}),iconEnd:e.jsx(t,{}),children:[{id:"012",label:"012 - eget aliquet nibh praesent tristique magna sit amet purus gravida",iconStart:e.jsx(i,{}),iconEnd:e.jsx(t,{}),children:[{id:"013",label:"013 - eget aliquet nibh praesent tristique magna sit amet purus gravida",iconStart:e.jsx(i,{}),iconEnd:e.jsx(t,{}),children:[{id:"014",label:"014 - eget magna fermentum iaculis eu non diam phasellus vestibulum lorem sed risus ultricies tristique nulla aliquet enim tortor",iconStart:e.jsx(i,{}),iconEnd:e.jsx(t,{}),children:[{id:"015",label:"015 - eget aliquet nibh praesent tristique magna sit amet purus gravida",iconStart:e.jsx(i,{}),iconEnd:e.jsx(t,{}),children:[{id:"016",label:"016 - habitasse platea dictumst quisque sagittis purus sit amet volutpat consequat",iconStart:e.jsx(i,{}),iconEnd:e.jsx(t,{}),children:[{id:"017",label:"017 - habitasse platea dictumst quisque sagittis purus sit amet volutpat consequat",iconStart:e.jsx(i,{}),iconEnd:e.jsx(t,{}),children:[{id:"018",label:"018 - lorem ipsum ipsum ipsum",iconStart:e.jsx(i,{}),iconEnd:e.jsx(t,{}),children:[{id:"019",label:"019 - quisque sagittis purus sit amet volutpat",iconStart:e.jsx(i,{}),iconEnd:e.jsx(t,{}),children:[{id:"020",label:"020 - quisque sagittis purus sit amet volutpat",iconStart:e.jsx(i,{}),iconEnd:e.jsx(t,{}),children:[{id:"021",label:"021 - quisque sagittis purus sit amet volutpat",iconStart:e.jsx(i,{}),iconEnd:e.jsx(t,{}),children:[{id:"022",label:"022 - quisque sagittis purus sit amet volutpat",iconStart:e.jsx(i,{}),iconEnd:e.jsx(t,{}),children:[{id:"023",label:"023 - quisque sagittis purus sit amet volutpat",iconStart:e.jsx(i,{}),iconEnd:e.jsx(t,{}),children:[{id:"024",label:"024 - quisque sagittis purus sit amet volutpat",iconStart:e.jsx(i,{}),iconEnd:e.jsx(t,{}),children:[{id:"025",label:"025 - quisque sagittis purus sit amet volutpat",iconStart:e.jsx(i,{}),iconEnd:e.jsx(t,{}),children:[{id:"026",label:"026 - quisque sagittis purus sit amet volutpat",iconStart:e.jsx(i,{}),iconEnd:e.jsx(t,{}),children:[{id:"027",label:"027 - quisque sagittis purus sit amet volutpat",iconStart:e.jsx(i,{}),iconEnd:e.jsx(t,{}),children:[{id:"028",label:"028 - quisque sagittis purus sit amet volutpat",iconStart:e.jsx(i,{}),iconEnd:e.jsx(t,{}),children:[{id:"029",label:"029 - quisque sagittis purus sit amet volutpat",iconStart:e.jsx(i,{}),iconEnd:e.jsx(t,{}),children:[{id:"030",label:"030 - quisque sagittis purus sit amet volutpat",iconStart:e.jsx(i,{}),iconEnd:e.jsx(t,{}),children:[{id:"031",label:"031 - quisque sagittis purus sit amet volutpat",iconStart:e.jsx(i,{}),iconEnd:e.jsx(t,{}),children:[{id:"032",label:"032 - quisque sagittis purus sit amet volutpat",iconStart:e.jsx(i,{}),iconEnd:e.jsx(t,{}),children:[{id:"033",label:"033 - quisque sagittis purus sit amet volutpat",iconStart:e.jsx(i,{}),iconEnd:e.jsx(t,{}),children:[{id:"034",label:"034 - quisque sagittis purus sit amet volutpat",iconStart:e.jsx(i,{}),iconEnd:e.jsx(t,{}),children:[{id:"035",label:"035 - quisque sagittis purus sit amet volutpat",iconStart:e.jsx(i,{}),iconEnd:e.jsx(t,{}),children:[{id:"036",label:"036 - quisque sagittis purus sit amet volutpat",iconStart:e.jsx(i,{}),iconEnd:e.jsx(t,{}),children:[{id:"037",label:"037 - quisque sagittis purus sit amet volutpat",iconStart:e.jsx(i,{}),iconEnd:e.jsx(t,{}),children:[{id:"038",label:"038 - quisque sagittis purus sit amet volutpat",iconStart:e.jsx(i,{}),iconEnd:e.jsx(t,{}),children:[{id:"039",label:"039 - quisque sagittis purus sit amet volutpat",iconStart:e.jsx(i,{}),iconEnd:e.jsx(t,{}),children:[{id:"040",label:"040 - quisque sagittis purus sit amet volutpat",iconStart:e.jsx(o,{}),iconEnd:e.jsx(t,{})}]}]}]}]}]}]}]}]}]}]}]}]}]}]}]}]}]}]}]}]}]}]}]}]}]}]}]}]}]}]}]}]}]}]}]}]}]},{id:"11",label:"label",iconStart:e.jsx(o,{})},{id:"12",label:"another label",iconStart:e.jsx(o,{})}]}]},{id:"R2",label:"0-2 level2",iconStart:e.jsx(o,{}),iconEnd:e.jsx(t,{})},{id:"R3",label:"0-3 level2",iconStart:e.jsx(o,{}),iconEnd:e.jsx(t,{})},{id:"R4",label:"0-4 level2",iconStart:e.jsx(o,{}),iconEnd:e.jsx(t,{})}]}],l=["01","02","03"],Je={title:"Layouts/Demos"},u=()=>{const[a,c]=h.useState([]),[n,m]=h.useState([]),f=s=>{a.includes(s.id)?c(a.filter(r=>r!==s.id)):c([s.id])},E=s=>{n.includes(s.id)?m(n.filter(r=>r!==s.id)):m([s.id])};return e.jsx("div",{style:{transform:"scale(1)"},children:e.jsx(g,{navigation:e.jsx(k,{children:"level 1"}),content:e.jsx(I,{navigation:e.jsx(H,{header:e.jsx(R,{children:"Header"}),children:e.jsxs(N,{isReversed:!0,defaultOpenedItem:l[1],children:[e.jsx(v,{id:l[0],icon:e.jsx(A,{size:"big"}),label:"Default tree",children:e.jsx(S,{isReversed:!0,data:y,selectedItems:a,onClickItem:f})}),e.jsx(v,{id:l[1],icon:e.jsx(P,{size:"big"}),label:"Nested",children:e.jsx(S,{isReversed:!0,data:F,selectedItems:n,onClickItem:E})})]})}),content:e.jsx(b,{header:e.jsx(q,{title:"Page title"}),content:e.jsxs(e.Fragment,{children:[e.jsx(d,{children:"Content"}),e.jsx(d,{children:C})]})})})})})},p=()=>{const[a,c]=h.useState([]),[n,m]=h.useState([]),f=s=>{a.includes(s.id)?c(a.filter(r=>r!==s.id)):c([s.id])},E=s=>{n.includes(s.id)?m(n.filter(r=>r!==s.id)):m([s.id])};return e.jsx("div",{style:{transform:"scale(1)"},children:e.jsx(g,{navigation:e.jsx(k,{children:"level 1"}),content:e.jsx(I,{navigation:e.jsx(H,{isReversed:!1,header:e.jsx(R,{children:"Header"}),children:e.jsxs(N,{defaultOpenedItem:l[1],children:[e.jsx(v,{id:l[0],icon:e.jsx(A,{size:"big"}),label:"Default tree",children:e.jsx(S,{data:y,selectedItems:a,onClickItem:f})}),e.jsx(v,{id:l[1],icon:e.jsx(P,{size:"big"}),label:"Nested",children:e.jsx(S,{data:F,selectedItems:n,onClickItem:E})})]})}),content:e.jsx(b,{header:e.jsx(q,{title:"Page title"}),content:e.jsxs(e.Fragment,{children:[e.jsx(d,{children:"Content"}),e.jsx(d,{children:C})]})})})})})},x=()=>e.jsx("div",{style:{transform:"scale(1)"},children:e.jsx(g,{navigation:e.jsx(L,{}),content:e.jsx(I,{navigation:e.jsx(D,{}),content:e.jsx(b,{isCentered:!0,header:e.jsx(q,{title:"Header"}),content:e.jsx(d,{children:"Content"})})})})}),j=()=>e.jsx("div",{style:{transform:"scale(1)"},children:e.jsx(g,{navigation:e.jsx(L,{}),content:e.jsx(I,{content:e.jsx(b,{header:e.jsx(q,{title:"Title"}),content:e.jsx(d,{className:"flexFluid",children:"Content"})})})})});u.__docgenInfo={description:"",methods:[],displayName:"Example"};p.__docgenInfo={description:"",methods:[],displayName:"ExampleLight"};x.__docgenInfo={description:"",methods:[],displayName:"Centered"};j.__docgenInfo={description:"",methods:[],displayName:"WithoutLevel2"};u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`() => {
+  const [selectedItems1, setSelectedItems1] = useState([]);
+  const [selectedItems2, setSelectedItems2] = useState([]);
+  const handleSelectItem1 = node => {
+    if (selectedItems1.includes(node.id)) {
+      setSelectedItems1(selectedItems1.filter(item => item !== node.id));
+    } else {
+      setSelectedItems1([node.id]);
+    }
+  };
+  const handleSelectItem2 = node => {
+    if (selectedItems2.includes(node.id)) {
+      setSelectedItems2(selectedItems2.filter(item => item !== node.id));
+    } else {
+      setSelectedItems2([node.id]);
+    }
+  };
+  return <div style={{
+    transform: 'scale(1)'
+  }}>
+            <LayoutApp navigation={<PrimaryNav>level 1</PrimaryNav>} content={<LayoutModule navigation={<SecondaryNav header={<SecondaryNavHeader>Header</SecondaryNavHeader>}>
+                    <Accordion isReversed defaultOpenedItem={accordionIds[1]}>
+                        <AccordionItem id={accordionIds[0]} icon={<Love size="big" />} label="Default tree">
+                            <TreeView isReversed data={treeData} selectedItems={selectedItems1} onClickItem={handleSelectItem1} />
+                        </AccordionItem>
+                        <AccordionItem id={accordionIds[1]} icon={<Bug size="big" />} label="Nested">
+                            <TreeView isReversed data={treeDataNested} selectedItems={selectedItems2} onClickItem={handleSelectItem2} />
+                        </AccordionItem>
+                    </Accordion>
+                </SecondaryNav>} content={<LayoutContent header={<Header title="Page title" />} content={<>
+                        <Paper>Content</Paper>
+                        <Paper>{lorem}</Paper>
+                    </>} />} />} />
+        </div>;
+}`,...u.parameters?.docs?.source}}};p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`() => {
+  const [selectedItems1, setSelectedItems1] = useState([]);
+  const [selectedItems2, setSelectedItems2] = useState([]);
+  const handleSelectItem1 = node => {
+    if (selectedItems1.includes(node.id)) {
+      setSelectedItems1(selectedItems1.filter(item => item !== node.id));
+    } else {
+      setSelectedItems1([node.id]);
+    }
+  };
+  const handleSelectItem2 = node => {
+    if (selectedItems2.includes(node.id)) {
+      setSelectedItems2(selectedItems2.filter(item => item !== node.id));
+    } else {
+      setSelectedItems2([node.id]);
+    }
+  };
+  return <div style={{
+    transform: 'scale(1)'
+  }}>
+            <LayoutApp navigation={<PrimaryNav>level 1</PrimaryNav>} content={<LayoutModule navigation={<SecondaryNav isReversed={false} header={<SecondaryNavHeader>Header</SecondaryNavHeader>}>
+                    <Accordion defaultOpenedItem={accordionIds[1]}>
+                        <AccordionItem id={accordionIds[0]} icon={<Love size="big" />} label="Default tree">
+                            <TreeView data={treeData} selectedItems={selectedItems1} onClickItem={handleSelectItem1} />
+                        </AccordionItem>
+                        <AccordionItem id={accordionIds[1]} icon={<Bug size="big" />} label="Nested">
+                            <TreeView data={treeDataNested} selectedItems={selectedItems2} onClickItem={handleSelectItem2} />
+                        </AccordionItem>
+                    </Accordion>
+                </SecondaryNav>} content={<LayoutContent header={<Header title="Page title" />} content={<>
+                        <Paper>Content</Paper>
+                        <Paper>{lorem}</Paper>
+                    </>} />} />} />
+        </div>;
+}`,...p.parameters?.docs?.source}}};x.parameters={...x.parameters,docs:{...x.parameters?.docs,source:{originalSource:`() => <div style={{
+  transform: 'scale(1)'
+}}>
+        <LayoutApp navigation={<FakePrimaryNavigation />} content={<LayoutModule navigation={<FakeSecondaryNavigation />} content={<LayoutContent isCentered header={<Header title="Header" />} content={<Paper>Content</Paper>} />} />} />
+    </div>`,...x.parameters?.docs?.source}}};j.parameters={...j.parameters,docs:{...j.parameters?.docs,source:{originalSource:`() => <div style={{
+  transform: 'scale(1)'
+}}>
+        <LayoutApp navigation={<FakePrimaryNavigation />} content={<LayoutModule content={<LayoutContent header={<Header title="Title" />} content={<Paper className="flexFluid">Content</Paper>} />} />} />
+    </div>`,...j.parameters?.docs?.source}}};const Ke=["Example","ExampleLight","Centered","WithoutLevel2"];export{x as Centered,u as Example,p as ExampleLight,j as WithoutLevel2,Ke as __namedExportsOrder,Je as default};
---
+++ assets/PrimaryNav.context-Q0MfoXRH.js
@@ -0,0 +1,1 @@
+import{r as t}from"./iframe-BeCTjTOk.js";const o=t.createContext({});export{o as P};
