f5725bd1006e9f9de1b135947a2d252c1483a208
Yacine Bouklif <ybouklif@jahia.com>
2026-02-23 14:21:43 +0100

    feat: add cross icon, accent styles, and tighter spacing in dropdown

--- src/components/Tag/Tag.tsx
+++ src/components/Tag/Tag.tsx
@@ -5,16 +5,19 @@ import type {TagProps} from './Tag.types';
 
 import './Tag.scss';
 import {Typography} from '~/components/Typography';
+import {Close} from '~/icons';
 
 export const Tag: React.FC<TagProps> = ({label, value, size = 'medium', onClick, className, isDisabled = false, ...props}) => (
     <button
         type="button"
         className={clsx('moonstone-tag', className, {'moonstone-small': size === 'small'})}
         disabled={isDisabled}
+        aria-label={`Remove ${label}`}
         onClick={e => onClick(e, value)}
         {...props}
     >
         {label && <Typography isNowrap component="span" variant="caption">{label}</Typography>}
+        <Close aria-hidden size={size === 'small' ? 'small' : 'default'} className="moonstone-tag_icon"/>
     </button>
 );
 
