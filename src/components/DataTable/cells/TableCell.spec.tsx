3d3a89162761ca639bb06a0d432101a20a3a99ef
Félix Vallé <fvalle@jahia.com>
2026-03-20 15:20:02 +0100

    fix: test

--- src/components/DataTable/cells/TableCell.spec.tsx
+++ src/components/DataTable/cells/TableCell.spec.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {render, screen} from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import {describe, it, expect} from 'vitest';
 import {TableCell} from './TableCell';
 
