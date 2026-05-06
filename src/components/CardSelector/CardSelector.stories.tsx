9647efaacaf5f06fba2d14740f9378a2e72de142
Félix Vallé <fvalle@jahia.com>
2026-05-06 09:22:09 +0200

    fix: wrong import component

--- src/components/CardSelector/CardSelector.stories.tsx
+++ src/components/CardSelector/CardSelector.stories.tsx
@@ -1,7 +1,7 @@
 import {StoryObj, Meta} from '@storybook/react-vite';
 
 import {CardSelector} from './index';
-import {Button, Chip} from '~/scoped';
+import {Button, Chip} from '~/components';
 import {Close, FileImage, Lock, Love} from '~/icons';
 import type {CardSelectorProps} from './CardSelector.types';
 
